e5fd0f98d385c937e8d58d5213a08a8757bedc3e
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-23 07:39:38 +0530

    4tett55

--- Commands/invite.js
+++ Commands/invite.js
@@ -8,7 +8,7 @@ module.exports = {
         let embed = new Discord.MessageEmbed()
         .setTitle("Get Bot's Invite Link")
         .setColor("RANDOM")
-        .addField(`Here you go: `, "[Click Me to invite](https://discordapp.com/oauth2/authorize?client_id=733670294086221865&scope=bot&permissions=8&response_type=code)")
+        .addField(`Here you go: `, "[Click Me to invite](https://discord.com/oauth2/authorize?client_id=733670294086221865&permissions=8&scope=bot)")
         .setFooter("Thanks for Inviting");
         message.channel.send(embed);
     }
