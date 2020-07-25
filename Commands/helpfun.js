c05d85ca94f3dd935ec666f45086e1041a0c40d7
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-25 08:26:26 +0530

    tfyfy

--- Commands/helpfun.js
+++ Commands/helpfun.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "Get Fun Commands",
     run: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
-        .addField("Fun Commands", "`8ball <message>` \n`amazeme` \n`ascii <text>` \n`cowsay <text>` \n`emojify <text>` \n`fliptext <text>` \n`roast <@user>` \n`trivia`")
+        .addField("Fun Commands", "`8ball <message>` \n`amazeme` \n`ascii <text>` \n`cowsay <text>` \n`emojify <text>` \n`fliptext <text>` \n'kill <@user>` \n`roast <@user>` \n`trivia`")
         .setColor("RANDOM");
 
         message.channel.send(embed);
