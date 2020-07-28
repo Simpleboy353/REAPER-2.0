a6e8b5689234d28f10cfb8c2ac06d628733d52a4
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-28 09:03:18 +0530

    Added Color to the embed message

--- Commands/botinfo.js
+++ Commands/botinfo.js
@@ -6,6 +6,7 @@ module.exports = {
     run: async (client, message, args) => {
         let embed = new Discord.MessageEmbed()
         .setTitle("Bot Info")
+        .setColor("RANDOM")
         .setDescription("Hello \nI am a multi-purpose Discord bot made by **_๖ۣۜℜⱥjͥƤuͣtͫ_#5915**. I am still under development and my developer is constantly adding new features to me. If you have any ques or wanna report any bug to dm my Developer.")
         .setFooter("Thanks, Reaper 2.0 Team");
         message.channel.send(embed);
