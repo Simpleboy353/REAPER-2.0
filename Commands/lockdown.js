40300e9aefb0e52da7224c4529424a06cac45de2
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-28 17:41:35 +0530

    Updated
    Completely changed Lockdown System

--- Commands/lockdown.js
+++ Commands/lockdown.js
@@ -1,34 +1,65 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const ms = require("ms");
 
-module.exports = {
-    name: "lockdown",
-    description: "Start or Stop Lockdown in any channel",
-    run: async (client, message, args) => {
-        [{
-            key: 'type',
-            prompt: 'Please enter either start or stop.',
-            type: 'string',
-            default: 'start',
-            validate: type => {
-                if (['start', 'stop'].includes(type.toLowerCase())) return true;
-                return 'Please enter either start or stop.';
-            },
-            parse: type => type.toLowerCase()
-        }]
-    if (!message.member.hasPermission("MANAGE_CHANNELS", "MANAGE SERVER")) {
-    return message.channel.send("You don't have enough Permissions to initiate a Lockdown")
-    }
-    if (!message.guild.me.hasPermission("MANAGE_CHANNELS", "MANAGE_SERVER")) {
-    return message.channel.send("I don't have enough Permissions to initiate a lockdown")
-    }
-    const { type } = args;
-        if (type === 'start') {
-            await message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: false }, `Lockdown initiated by ${message.author.tag}`);
-            return message.channel.send(`Lockdown has initiated! Most users are now unable to send a message in this channel!\n\Please use \`lockdown stop\` to end the lockdown!`);
+exports.run = async (client, message, [time, reason]) => {
+  if (!client.lockit) { client.lockit = []; }
+  let validUnlocks = ["release", "unlock", "u"];
+  if (!time) { return message.reply("Please enter a valid period in s; m; h; d"); }
 
-        } else if (type === 'stop') {
-            await message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: null }, `Lockdown terminated by ${message.author.tag}`);
-            return message.channel.send('Lockdown ended!');
-        }
-    }
-    };
+  const Lockembed = new Discord.MessageEmbed()
+    .setColor(0xDD2E44)
+    .setTimestamp()
+    .setTitle("🔒 MOD LOCK [LOCKDOWN] 🔒")
+    .setDescription(`This Channel has been lock by ${message.author.tag} for ${time}`);
+    if (reason != null) { Lockembed.addField("Reason : ", reason); }
+
+  const Unlockembed = new Discord.MessageEmbed()
+    .setColor(0xDD2E44)
+    .setTimestamp()
+    .setTitle("🔓 MOD LOCK [UNLOCK] 🔓")
+    .setDescription("This channel is now unlock");
+
+  if (message.channel.permissionsFor(message.author.id).has("MUTE_MEMBERS") === false) { 
+    const embed = new new Discord.MessageEmbed()
+      .setColor(0xDD2E44)
+      .setTimestamp()
+      .setTitle("❌ ERROR ❌")
+      .setDescription("You can\'t do that");
+    return message.channel.send({embed});  
+  }  
+
+  if (validUnlocks.includes(time)) {
+    message.channel.overwritePermissions(message.guild.id, { SEND_MESSAGES: null }).then(() => {
+      message.channel.send({embed: Unlockembed});
+      clearTimeout(client.lockit[message.channel.id]);
+      delete client.lockit[message.channel.id];
+    }).catch(error => { console.log(error); });
+  } else {
+    message.channel.overwritePermissions(message.guild.id, { SEND_MESSAGES: false }).then(() => {
+      message.channel.send({embed: Lockembed}).then(() => {
+        client.lockit[message.channel.id] = setTimeout(() => {
+          message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: null
+          }).then(message.channel.send({embed: Unlockembed})).catch(console.error);
+          delete client.lockit[message.channel.id];
+        }, ms(time));
+      }).catch(error => { console.log(error); });
+    });
+  }
+};
+
+exports.conf = {
+  enabled: true,
+  runIn: ["text"],
+  aliases: ["ld", "lock"],
+  permLevel: 2,
+  botPerms: ["MANAGE_ROLES", "EMBED_LINKS", "ADMINISTRATOR"]
+};
+  
+exports.help = {
+  name: "modlock",
+  description: "Locks or unlocks the channel.",
+  usage: "[time:str] [reason:str]",
+  usageDelim: " | "
+};
