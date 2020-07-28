5f20bbe47b7c336fa26fb6f30b39690140584b51
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-28 08:59:56 +0530

    nihr8grpiurp

---
+++ Commands/botinfo.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "botinfo",
+    description: "Shows the bot info",
+    run: async (client, message, args) => {
+        let embed = new Discord.MessageEmbed()
+        setTitle("Bot Info")
+        setDescription("Hello \nI am a multi-purpose Discord bot made by **_๖ۣۜℜⱥjͥƤuͣtͫ_#5915**. I am still under development and my developer is constantly adding new features to me. If you have any ques or wanna report any bug to dm my Developer.")
+        setFooter("Thanks, Reaper 2.0 Team");
+        message.channel.send(embed);
+    }
+}
