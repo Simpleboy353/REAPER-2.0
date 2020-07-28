a26fb977fcf478ca7d23e512d6df08f7fb02ea6f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-28 19:02:13 +0530

    Update
    Added color to the Embed message

--- Commands/helpconfig.js
+++ Commands/helpconfig.js
@@ -5,6 +5,7 @@ module.exports = {
     description: "Get list of Configuration commands",
     run: async(client, message, args) => {
     const embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
     .addField("Configuration Commands", "`prefix`");
     message.channel.send(embed);
    }
