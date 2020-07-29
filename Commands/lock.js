2bcf832e00c67f523f9b140b519f5c1a9d88b3d4
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-29 19:49:25 +0530

    gb fs vfs

--- Commands/lock.js
+++ Commands/lock.js
@@ -4,7 +4,7 @@ module.exports={
     name: "lock",
     description: "Locks a channel",
     run: async(client, message, args) => {
-    await message.channel.overwritePermissions.update(message.guild.defaultRole, { SEND_MESSAGES: false });
+    await message.channel.permissionOverwrites.update(message.guild.defaultRole, { SEND_MESSAGES: false });
     return message.channel.send(`Lockdown has initiated! Most users are now unable to send a message in this channel`);
     }
 }
