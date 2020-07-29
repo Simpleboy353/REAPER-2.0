9f6cff7d47bd60a0e9eae5b9e27af3d00f234733
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-29 13:15:05 +0530

    Added intervals

--- Commands/hack.js
+++ Commands/hack.js
@@ -11,13 +11,13 @@ module.exports = {
     if(tag.id === message.author.id){
       return message.channel.send(`${message.author.username} I cant hack you , tell someone to hack`)
     }
-    const a = await message.channel.send(`Hacking ${tag}`)
+    const a = await message.channel.send((`Hacking ${tag}`), 2000)
     const b = await a.edit("Getting User's Personal info....")
-    const c = await b.edit("Getting User's Personal info.")
-    const d = await c.edit("Getting User's Personal info..")
-    const e = await d.edit("Getting User's Personal info...")
-    const f = await e.edit("Done Getting User's info")
-    const g = await f.edit("Selling the data to FBI")
+    const c = await b.edit(("Getting User's Personal info."), 3000)
+    const d = await c.edit(("Getting User's Personal info.."), 3000)
+    const e = await d.edit(("Getting User's Personal info..."), 3000)
+    const f = await e.edit(("Done Getting User's info"), 3000)
+    const g = await f.edit(("Selling the data to FBI"), 3000)
     let random = Math.floor(Math.random() * 500 )
     const h = await g.edit(`Sold for ${random}$ to the fbi`)
 
