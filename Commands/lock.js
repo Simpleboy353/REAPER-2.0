de433168f0bc7c5468a2a056e3ad372dcca5a476
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-29 19:24:08 +0530

    rgrw

--- Commands/lock.js
+++ Commands/lock.js
@@ -6,11 +6,11 @@ module.exports={
     run: async(client, message, args) =>{ 
     if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`⛔ Missing permission: \`Administrator\``); 
     try{ 
-        var message = await message.channel.send(`Locking this channel...`); 
      await message.channel.overwritePermissions(message.guild.id, {
-        SEND_MESSAGES: false 
+        SEND_MESSAGES: false
      }).then(() =>{ 
         }); 
+        var message = message.channel.send(`This channel Has been locked by ${message.auhtor.username}`)
     }catch(e){ 
     return message.channel.send(`⛔ Error: `+e) 
 }
