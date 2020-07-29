26a3af6669595a0b99fcaab822f013fedd980c29
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-29 12:21:46 +0530

    Updated

--- Commands/unlock.js
+++ Commands/unlock.js
@@ -4,16 +4,13 @@ module.exports = {
     name:"unlock",
     description: "Unlocks a Channel",
     run: async (client, message, args) =>{
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`:no_entry: Missing permission: \`Administrator\``);
-    try{
-        var message = await message.channel.send(`Starting the Channel Unlock Process.....`);
+    if(!message.member.hasPermission('ADMINISTRATOR')) {
+        return message.channel.send(`You don't have enough Permission to Lock a Channel`);
+    }
         await message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: null
         }).then(() =>{
-            message.edit('The Channel has been Unlocked Successfully!');
+            message.channel.send('The Channel has been Unlocked Successfully!');
         });
-    }catch(e){
-        return message.channel.send(`:unlock: Error: `+e)
     }
-}
-}
+    }
