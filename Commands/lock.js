11220a9e1600f04853d1cb8b2428db62c9fabfff
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-29 19:35:14 +0530

    Updated

--- Commands/lock.js
+++ Commands/lock.js
@@ -3,16 +3,8 @@ const Discord = module.require("discord.js");
 module.exports={
     name: "lock",
     description: "Locks a channel",
-    run: async(client, message, args) =>{ 
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`⛔ Missing permission: \`Administrator\``); 
-    try{ 
-     await message.channel.overwritePermissions(message.guild.id, {
-        SEND_MESSAGES: false
-     }).then(() =>{ 
-        }); 
-        var message = message.channel.send(`This channel Has been locked by ${message.auhtor.username}`)
-    }catch(e){ 
-    return message.channel.send(`⛔ Error: `+e) 
-}
-}
+    run: async(client, message, args) => {
+    await message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: false });
+    return message.channel.send(`Lockdown has initiated! Most users are now unable to send a message in this channel`);
+    }
 }
