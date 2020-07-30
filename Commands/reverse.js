cdbd67a1c026f9aa2ea284007be2aea785064eab
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-30 18:54:55 +0530

    Update

--- Commands/reverse.js
+++ Commands/reverse.js
@@ -2,16 +2,12 @@ const Discord = module.require("discord.js");
 
 module.exports = {
     name: "reverse",
-    description: "Reverse  the text entered",
+    description: "Reverse the text entered",
     run: async(client, message, args) => {
-        try {
-            if (!args[0]) return this.client.embed("commonError", message, "Please provide some text for me search from reverse.");
-            const str = args.join(' ');
-            let msg = await message.channel.send(str.split('').reverse().join(''));
-            msg.react('🔁');
-        } catch(err) {
-            this.client.logger.error(err.stack);
-            return this.client.embed("", message);
-        }
+       let str = args.join(" ");
+       if (!str) {
+       return message.channel.send("Enter some text to be reversed")
+       }
+       message.channel.send(str.split('').reverse().join(''));
     }
 }
