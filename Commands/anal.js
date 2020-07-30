f72d00697ff6799899eac0cd84bb660b20ab92bb
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-30 09:45:06 +0530

    fixed

--- Commands/anal.js
+++ Commands/anal.js
@@ -4,16 +4,17 @@ module.exports = {
     name: "anal",
     description: "NFSW Commands",
     run: async(client, message, args) => {
-        if (!message.channel.nsfw) return message.channel.send('You can use this command in an NSFW Channel!')
-        superagent.get('https://nekos.life/api/v2/img/anal')
-            .end((err, response) => {
+        if (!message.channel.nsfw) {
+        return message.channel.send('You can use this command in an NSFW Channel!')
+        }
+        var img = superagent.get('https://nekos.life/api/v2/img/anal')
+
           const lewdembed = new Discord.MessageEmbed()
           .setTitle("Hentai")
-          .setImage(response.body.url)
+          .setImage(img.body.url)
           .setColor(`#000000`)
           .setFooter(`Tags: anal`)
           .setURL(response.body.url);
         message.channel.send(lewdembed);
-    })
-}
+    }
 }
