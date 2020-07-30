077c33d52850b44299c327e0fd42585fc2fa6b43
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-30 08:25:39 +0530

    Updated Poll command

--- Commands/poll.js
+++ Commands/poll.js
@@ -4,28 +4,21 @@ module.exports = {
     name: "poll",
     description: "Start a Poll",
     run: async (client, message, args) => {
-
-	if (!message.member.roles.cache.find("name", "@everyone")) { //Whatever role you want, I pick @everyone because everyone can use this command
-		message.channel.send('Invalid permissions.');
-		return;
-	}
-    
-    // Check for input
-    if (!args[0]) return message.channel.send('Proper usage: poll <question>');
-    
-    // Create Embed
-    const embed = new Discord.MessageEmbed()
-        .setColor("RANDOM") //To change color do .setcolor("#fffff")
-        .setTitle('React to Vote.')
-        .setDescription(args.join(' '))
-        .setFooter(`Poll Created By ${message.author.username}`);
-        
-    let msg = await message.channel.send(embed)
-        .then(function (msg) {
-            msg.react("👍");
-            msg.react("👎"); // You can only add two reacts
-            message.delete({timeout: 1000});
-            }).catch(function(error) {
-            console.log(error);
-        });
-}};
+    if (!message.member.hasPermission('MANAGE_SERVER')) {
+    return message.channel.send("You don't have enough Permissions")
+    }
+    if (args === 0) {
+    return message.channel.send("Enter some text for the Poll")
+    }
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Poll Time")
+    .setDescription(`${args}`.split(',').join(' '))
+    .setFooter(`Started by ${message.author.username}`)
+    .setColor("RANDOM");
+    message.channel.send(embed)
+    .then(function (message, str) {
+        message.react("👍")
+        message.react("👎")
+      }).catch(function () {});
+}
+}
