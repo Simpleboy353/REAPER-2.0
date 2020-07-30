a9161d426bcd50f83dbc6a41fd2f03fe5ba1360c
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-30 19:03:59 +0530

    Updated UI

--- Commands/helpmod.js
+++ Commands/helpmod.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "Get Moderation Commands",
     run: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
-        .addField("Moderation Commands", "`kick <@user <reason>` \n`ban <@user> <reason>` \n`softban <@user> <reason>` \n`mute <@user> <reason>` \n`unmute <@user> <reason>` \n`tempmute <@user> <reason>` \n`warn <@user> <reason>`")
+        .addField("Moderation Commands", "`kick`, `ban`, `softban`, `mute`, `unmute`, `tempmute`, `warn`")
         .setColor("RANDOM");
 
         message.channel.send(embed);
--- Commands/helputility.js
+++ Commands/helputility.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "Get Utility Commands",
     run: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
-        .addField("Utility Commands", "`avatar` \n`announce <message>` \n`clear <amount>` \n`createrole`\n`giverole <@user> <rolename>` \n`nickname <@user> <newname>` \n`poll <text>` \n`removerole <@user> <rolename>` \n`say <message>`")
+        .addField("Utility Commands", "`avatar`, `announce`, `clear`, `createrole`, `giverole`, `nickname`, `poll`, `removerole`, `say`")
         .setColor("RANDOM");
 
         message.channel.send(embed);
