de1b8d351be3c7a283a8339a055bad57fdc8a53f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-07-31 08:44:01 +0530

    Fixed Errors

--- Commands/urban.js
+++ Commands/urban.js
@@ -4,24 +4,24 @@ module.exports = {
     name: "urban",
     description: "Find meanings from the urban dictionary",
     run: async(client, message, args) => {
-        let str = args.join("");
-  
-  if (args.length > 0) {
-    urban(str).first(json => {
-      if (!json) return message.channel.send("No results found!")
-      
-      let embed = new Discord.RichEmbed()
-        .setTitle(json.word)
-        .setColor("3498db")
-        .setDescription(json.definition || "No definition!")
-        .addField("Upvotes", json.thumbs_up, true)
-        .addField("Downvotes", json.thumbs_down, true)
-        .setFooter("Source: Urban Dictionary, Author: " + json.author);
-      
-      message.channel.send(embed=embed);
-    });
-  } else {
-    await message.channel.send("Enter a word!").then(msg => msg.delete(2300));
+    let str = args.join("");
+    if (!str) {
+    return message.channel.send("Enter a word")
     }
+    if (args.length > 0) {
+    urban(str).first(json => {
+    if (!json) return message.channel.send("No results found!")
+      
+    let embed = new Discord.RichEmbed()
+    .setTitle(json.word)
+    .setColor("3498db")
+    .setDescription(json.definition || "No definition!")
+    .addField("Upvotes", json.thumbs_up, true)
+    .addField("Downvotes", json.thumbs_down, true)
+    .setFooter("Source: Urban Dictionary, Author: " + json.author);
+      
+    message.channel.send(embed);
+    });
+  }
 }
 }
