0d7d52ce8ce2a95561ccf42793d88b7774f329cd
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-01 17:19:44 +0530

    Fixed errors

--- Commands/salty.js
+++ Commands/salty.js
@@ -5,10 +5,13 @@ module.exports = {
     description: "Another fun command",
     run: async(client, message, args) => {
     let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL({ format: 'png', dynamic: true, size: 2048 }) : message.author.avatarURL({ format: 'png', dynamic: true, size: 2048 });
-
+    let salty = `https://api.alexflipnote.dev/salty?image=${avatar}` + `.gif`;
     if (!avatar) {
         return message.channel.send("You need to mention someone")
         }
-        message.channel.send({files : [{attachment: `https://api.alexflipnote.dev/salty?image=${avatar}`, name: 'file.gif'}]})
+        let embed = new Discord.MessageEmbed()
+        .setImage(salty)
+        .setColor("RANDOM");
+        message.channel.send(embed);
     }
 }
