779590adbbf998c7a1f7cf976897989130d6f686
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-01 18:10:31 +0530

    Fixed Errors

--- Commands/bubblewrap.js
+++ Commands/bubblewrap.js
@@ -7,6 +7,6 @@ module.exports = {
     if (!args[0]) {
     return message.channel.send("You need to enter at least one word and not more than 20 words")
     }
-    await message.channel.send(`||${args[0]}|| ||${args[1]}|| ||${args[2]}|| ||${args[3]}|| ||${args[4]}|| ||${args[5]}|| ||${args[6]}|| ||${args[7]}|| ||${args[8]}|| ||${args[9]}|| ||${args[10]}|| ||${args[11]}|| ||${args[12]}|| ||${args[13]}|| ||${args[14]}|| ||${args[15]}|| ||${args[16]}|| ||${args[17]}|| ||${args[18]}|| ||${args[19]}||`);
+    await message.channel.send(`||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O|| ||O||`);
     }
 }
