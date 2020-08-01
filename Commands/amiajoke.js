6bf8ee090bec6a92ab01cc2fb0150f51bad2693a
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-01 13:26:59 +0530

    Fixed bugs

--- Commands/amiajoke.js
+++ Commands/amiajoke.js
@@ -5,11 +5,11 @@ module.exports = {
     description: "Another fun command",
     run: async(client, message, args) => {
     let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL({ format: 'png', dynamic: true, size: 2048 }) : message.author.avatarURL({ format: 'png', dynamic: true, size: 2048 });
-    if (!avatar) {
-    return message.channel.send("You need to mention someone")
-    }
     let link = `https://api.alexflipnote.dev/amiajoke?image=${avatar}`;
 
+    if (!avatar) {
+        return message.channel.send("You need to mention someone")
+        }
     const embed = new Discord.MessageEmbed()
     .setColor("#800080")
     .setImage(link);
