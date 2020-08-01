2aedd6a2c58324f590a446bfdcf0749ab36b46f3
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-01 12:03:55 +0530

    Hoping For the errors to fix

--- Commands/scroll.js
+++ Commands/scroll.js
@@ -7,15 +7,10 @@ module.exports = {
     description: "Another Fun Command",
     run: async(client, message, args) => {
             if (message.channel.type === "dm") return;
-            let text = args.join(' ');
-            let scroll = `https://api.alexflipnote.dev/scroll?text=${text}` + `.png`;
+            let text = args.join(' ')
             if (!text) {
                 return message.reply('You need to provide some text!');
             }
-            const emb = new Discord.MessageEmbed();
-            emb.setImage(scroll);
-            message.channel.send({
-                embed: emb
-            })
+            message.channel.send({files : [{attachment: `https://api.alexflipnote.dev/scroll?text=${text}`, name: 'file.jpg'}]})
     }
 };
