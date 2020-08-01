546dcac1a694f1dd965f5a27a1886080057031ff
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-01 13:23:36 +0530

    Updated and fixed bugs

--- Commands/amiajoke.js
+++ Commands/amiajoke.js
@@ -5,6 +5,9 @@ module.exports = {
     description: "Another fun command",
     run: async(client, message, args) => {
     let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL({ format: 'png', dynamic: true, size: 2048 }) : message.author.avatarURL({ format: 'png', dynamic: true, size: 2048 });
+    if (!avatar) {
+    return message.channel.send("You need to mention someone")
+    }
     let link = `https://api.alexflipnote.dev/amiajoke?image=${avatar}`;
 
     const embed = new Discord.MessageEmbed()
--- Commands/jokeoverhead.js
+++ Commands/jokeoverhead.js
@@ -1,10 +1,13 @@
 const Discord = module.require("discord.js");
 
 module.exports = {
-    name: "jokeroverhead",
+    name: "jokeoverhead",
     description: "Another fun command",
     run: async(client, message, args) => {
     let avatar = message.mentions.users.size ? message.mentions.users.first().avatarURL({ format: 'png', dynamic: true, size: 2048 }) : message.author.avatarURL({ format: 'png', dynamic: true, size: 2048 });
+    if (!avatar) {
+    return message.channel.send("You need to mention someone")
+    }
     let link = `https://api.alexflipnote.dev/jokeoverhead?image=${avatar}`;
 
     const embed = new Discord.MessageEmbed()
