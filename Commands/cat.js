437d4858e2f4348739880e0ad34bb3c0eadf818f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-05 20:25:49 +0530

    Fixed errors

--- Commands/cat.js
+++ Commands/cat.js
@@ -1,14 +1,12 @@
 const Discord = module.require("discord.js");
+const Jimp = require("jimp");
+const cooldown = new Set();
 
 module.exports = {
     name: "cat",
-    description: "Another fun command",
+    description: "Another Fun Command",
     run: async(client, message, args) => {
-    let link = `https://api.alexflipnote.dev/cats`;
-
-    const embed = new Discord.MessageEmbed()
-    .setColor("#800080")
-    .setImage(link);
-    message.channel.send(embed);
+            if (message.channel.type === "dm") return;
+            message.channel.send({files : [{attachment: `https://api.alexflipnote.dev/cats`, name: 'file.jpg'}]})
     }
-}
+};
