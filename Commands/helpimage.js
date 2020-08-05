c5e3ee5a70fe182306e0b7dfc1f0f0bba33fc07f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-05 21:19:32 +0530

    Updated

---
+++ Commands/helpimage.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "helpimage",
+    description: "Get Image Manipulation commands",
+    run: async(client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTitle("Image Commands")
+    .setDescription("`achievement`, `amazeme`, `amiajoke`, `bad`, `challenge`, `didyoumean`, `drake`, `facts`, `jokeoverhead`, `pornhub`, `scroll`, `textimage`");
+    message.channel.send(embed);
+   }
+}
