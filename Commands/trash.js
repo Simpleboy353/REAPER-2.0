2a2814f54a42436d157ecc327515141620a56c43
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-06 14:31:56 +0530

    New Image Manipulation Command

---
+++ Commands/trash.js
@@ -0,0 +1,19 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+    name: "trash",
+    description: "Another fun command",
+    run: async(client, message, args) => {
+    let avatar = message.author.avatarURL({ format: 'png', dynamic: true, size: 2048 });
+    let target = message.mentions.users.first().avatarURL({ format: 'png', dynamic: true, size: 2048 });
+    if (!target) {
+    return message.channel.send("You need to mention someone")
+    }
+    let link = `https://api.alexflipnote.dev/trash?face=${avatar}&trash=${target}`;
+
+    const embed = new Discord.MessageEmbed()
+    .setColor("#800080")
+    .setImage(link);
+    message.channel.send(embed);
+    }
+}
