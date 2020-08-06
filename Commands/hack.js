691388c288d544c611eb3107de478477e809bed9
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-06 21:24:24 +0530

    Added Fun Command

---
+++ Commands/hack.js
@@ -0,0 +1,36 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+	name: "hack",
+	description: "Another Fun Command",
+	run: async(client, message, args) => {
+		const hacked = message.mentions.members.first();
+		if (!hacked) {
+		return message.channel.send("Woah..... Slow down, Who are we hacking..?")
+		}
+		const prompt = await msg.channel.createMessage(`Hacking ${user ? hacked.username : hacked} now...`)
+    await Memer.sleep(1500)
+    if (user) {
+      await prompt.edit('Finding discord login...')
+      await Memer.sleep(1700)
+      await prompt.edit(`Found:\n**Email**: \`${hacked.username}***@gmail.com\`\n**Password**: \`*******\``)
+      await Memer.sleep(1700)
+      await prompt.edit('Fetching dms')
+      await Memer.sleep(1000)
+      await prompt.edit('Listing most common words...')
+      await Memer.sleep(1000)
+      await prompt.edit(`Injecting virus into discriminator #${hacked.discriminator}`)
+      await Memer.sleep(1000)
+      await prompt.edit('Virus injected')
+      await Memer.sleep(1000)
+    }
+    await prompt.edit('Finding IP address')
+    await Memer.sleep(2000)
+    await prompt.edit('Spamming email...')
+    await Memer.sleep(1000)
+    await prompt.edit('Selling data to facebook...')
+    await Memer.sleep(1000)
+    await prompt.edit(`Finished hacking ${user ? hacked.username : hacked}`)
+    return 'The hack is complete.'
+		}
+	}
