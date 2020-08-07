d61024f517fc3bc5737166d4cb0d3350c0af7f49
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 13:33:25 +0530

    Updated

--- Commands/time.js
+++ Commands/time.js
@@ -5,11 +5,12 @@ module.exports = {
 	description: "Just for testing",
 	run: async(client, message, args) => {
                 const test = new Discord.MessageEmbed()
+                .setColor("RANDOM")
 		.setDescription("Starting Test");
                 message.channel.send(test);
-		message.test.edit(("First line"), 2000)
-		message.test.edit(("Second line"), 2000)
-		message.test.edit(("Last message"), 2000)
-		message.test.edit(("Finished Test"), 2000)
+		message.edit.test(("First line"), 2000)
+		message.edit.test(("Second line"), 2000)
+		message.edit.test(("Last message"), 2000)
+		message.edit.test(("Finished Test"), 2000)
     }
 }
