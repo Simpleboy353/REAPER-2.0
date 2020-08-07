5913a03998920df1b6afde0e8688cd388a12e06f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 14:15:23 +0530

    Updated With MS package

--- Commands/hack.js
+++ Commands/hack.js
@@ -1,36 +1,51 @@
-const Discord = module.require("discord.js");
+const ms = module.require("ms");
 
 module.exports = {
-	name: "hack",
-	description: "Another Fun Command",
-	run: async(client, message, args, Memer) => {
-		const hacked = message.mentions.members.first();
-		if (!hacked) {
-		return message.channel.send("Woah..... Slow down, Who are we hacking..?")
-		}
-		const prompt = await message.channel.send(`Hacking ${hacked.displayName} now...`)
-    await Memer.setInterval(1500)
-    if (hacked) {
-      await prompt.edit('Finding discord login...')
-      await Memer.setInterval(1700)
-      await prompt.edit(`Found:\n**Email**: \`${hacked.username}***@gmail.com\`\n**Password**: \`*******\``)
-      await Memer.setInterval(1700)
-      await prompt.edit('Fetching dms')
-      await Memer.setInterval(1000)
-      await prompt.edit('Listing most common words...')
-      await Memer.setInterval(1000)
-      await prompt.edit(`Injecting virus into discriminator #${hacked.discriminator}`)
-      await Memer.setInterval(1000)
-      await prompt.edit('Virus injected')
-      await Memer.setInterval(1000)
+  name: "hack",
+  description: "Another Fun Command",
+  run: async(client, message, args) => {
+    if (!args[0]) {
+    return message.channel.send("Woah.... Slow Down!! Who are we hacking..??")
     }
-    await prompt.edit('Finding IP address')
-    await Memer.setInterval(2000)
-    await prompt.edit('Spamming email...')
-    await Memer.setInterval(1000)
-    await prompt.edit('Selling data to facebook...')
-    await Memer.setInterval(1000)
-    await prompt.edit(`Finished hacking ${hacked.displayName}`)
-    return 'The hack is complete.'
-		}
-	}
+    const tohack = args.slice(0).join(" ") && args.shift().toLowerCase()
+    let msg = await message.channel.send("Hacking ${tohack}....")
+    let time = '3s'
+    setTimeout(function(){
+    msg.edit("Finding ${tohack}'s Email and Password.....")
+  }, ms(time)
+    let time1 = '4s'
+    setTimeout(function(){
+    msg.edit("E-Mail: `${tohack}@gmail.com \nPassword: `********`")
+  }, ms(time1)
+    let time2 = '2s'
+    setTimeout(function(){
+    msg.edit("Finding Other Accounts.....")
+  }, ms(time2)
+    let time3 = '3s'
+    setTimeout(function(){
+    msg.edit("Setting up Epic Games Account.....")
+  }, ms(time3)
+    let time4 = '3s'
+    setTimeout(function(){
+    msg.edit("Hacking Epic Games Account......")
+  }, ms(time4)
+    let time5 = '2s'
+    setTimeout(function(){
+    msg.edit("Hacked Epic Games Account!!")
+  }, ms(time5)
+    let time6 = '1s'
+    setTimeout(function(){
+    msg.edit("Collecting Info.....")
+  }, ms(time6)
+    let time7 = '3s'
+    setTimeout(function(){
+    msg.edit("Selling data to FBI....")
+  }, ms(time7)
+    let time8 = '2s'
+    setTimeout(function(){ 
+    msg.edit("Finished Hacking ${tohack}")
+  }, ms(time8)
+
+  message.channel.send("Finished Hacking ${tohack}")
+}
+}
