c7fff84cec0642e0937e9afd0c0287f2bd46e86f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 11:02:13 +0530

    Fixed Bugs

--- Commands/helpinfo.js
+++ Commands/helpinfo.js
@@ -5,7 +5,8 @@ module.exports = {
     description: "Get Info Commands",
     run: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
-        .addField("Info Commands", "`help`, `invite`, `ping`, `userinfo`, `userid`, `serverinfo`")
+        .setTitle("Info Commands")
+        .setDescription("`help`, `invite`, `ping`, `userinfo`, `userid`, `serverinfo`")
         .setColor("RANDOM");
 
         message.channel.send(embed);
