fe50d9f9fbc0972081a7e3175d7311c99719156c
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 14:37:05 +0530

    Updated

--- Commands/hack.js
+++ Commands/hack.js
@@ -10,12 +10,12 @@ module.exports = {
     const tohack = args.slice(0).join(" ") && args.shift().toLowerCase()
     let msg = await message.channel.send(`Hacking ${tohack}....`);
 
-    let time = '3s'
+    let time = '1s'
     setTimeout(function(){
     msg.edit(`Finding ${tohack}'s Email and Password.....`);
   }, ms(time));
 
-    let time1 = '4s'
+    let time1 = '2s'
     setTimeout(function(){
     msg.edit(`E-Mail: ${tohack}@gmail.com \nPassword: ********`);
   }, ms(time1));
@@ -35,22 +35,22 @@ module.exports = {
     msg.edit("Hacking Epic Games Account......");
   }, ms(time4));
 
-    let time5 = '2s'
+    let time5 = '3s'
     setTimeout(function(){
     msg.edit("Hacked Epic Games Account!!");
   }, ms(time5));
 
-    let time6 = '1s'
+    let time6 = '3s'
     setTimeout(function(){
     msg.edit("Collecting Info.....");
   }, ms(time6));
 
-    let time7 = '3s'
+    let time7 = '4s'
     setTimeout(function(){
     msg.edit("Selling data to FBI....");
   }, ms(time7));
 
-    let time8 = '2s'
+    let time8 = '4s'
     setTimeout(function(){ 
     msg.edit("Finished Hacking ${tohack}");
   }, ms(time8));
