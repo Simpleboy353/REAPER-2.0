9789cd285194188c838cdba21b20504aa42a0503
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 14:41:01 +0530

    Updated

--- Commands/hack.js
+++ Commands/hack.js
@@ -7,50 +7,50 @@ module.exports = {
     if (!args[0]) {
     return message.channel.send("Woah.... Slow Down!! Who are we hacking..??")
     }
-    const tohack = args.slice(0).join(" ") && args.shift().toLowerCase()
-    let msg = await message.channel.send(`Hacking ${tohack}....`);
+    const tohack = message.mentions.members.first()
+    let msg = await message.channel.send(`Hacking ${tohack.displayName}....`);
 
     let time = '1s'
     setTimeout(function(){
-    msg.edit(`Finding ${tohack}'s Email and Password.....`);
+    msg.edit(`Finding ${tohack.displayName}'s Email and Password.....`);
   }, ms(time));
 
     let time1 = '2s'
     setTimeout(function(){
-    msg.edit(`E-Mail: ${tohack}@gmail.com \nPassword: ********`);
+    msg.edit(`E-Mail: ${tohack.displayName}@gmail.com \nPassword: ********`);
   }, ms(time1));
 
-    let time2 = '2s'
+    let time2 = '3s'
     setTimeout(function(){
     msg.edit("Finding Other Accounts.....");
   }, ms(time2));
 
-    let time3 = '3s'
+    let time3 = '3.5s'
     setTimeout(function(){
     msg.edit("Setting up Epic Games Account.....");
   }, ms(time3));
 
-    let time4 = '3s'
+    let time4 = '4s'
     setTimeout(function(){
     msg.edit("Hacking Epic Games Account......");
   }, ms(time4));
 
-    let time5 = '3s'
+    let time5 = '4.5s'
     setTimeout(function(){
     msg.edit("Hacked Epic Games Account!!");
   }, ms(time5));
 
-    let time6 = '3s'
+    let time6 = '5s'
     setTimeout(function(){
     msg.edit("Collecting Info.....");
   }, ms(time6));
 
-    let time7 = '4s'
+    let time7 = '5.5s'
     setTimeout(function(){
     msg.edit("Selling data to FBI....");
   }, ms(time7));
 
-    let time8 = '4s'
+    let time8 = '6s'
     setTimeout(function(){ 
     msg.edit("Finished Hacking ${tohack}");
   }, ms(time8));
