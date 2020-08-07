7dcba25fa6b995c3c9e7da0e79f33b43bf640733
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 09:18:44 +0530

    Added another fun command

---
+++ Commands/infinite.js
@@ -0,0 +1,34 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+	name: "infinite",
+	description: "Another fun command",
+	run: async(client, message, args) => {
+		let msg = args.join(" ");
+		if (!msg) {
+		return message.channel.send("Error : No Text Found")
+        }
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send(`${msg}`)
+        message.channel.send("Looks like an infinite loop has been created \nPlease wait untill I fix it!")
+	message.channel.send("Done!! Fixed!!"), 2000)
+}
+}
