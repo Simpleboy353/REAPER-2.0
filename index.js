363149361f6ffd89ce6abeca7525731433e5d260
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 16:53:32 +0530

    Updated

--- index.js
+++ index.js
@@ -84,15 +84,15 @@ const command = args.shift().toLowerCase();
   } else if (command === 'userinfo') {
   client.commands.get('userinfo').run(client, message, args);
   }
-  if (command === 'h mod') {
-  client.commands.get('h mod').run(client, message, args);
-  } else if (command === 'h fun') {
-  client.commands.get('h fun').run(client, message, args);
+  if (command === 'helpmod') {
+  client.commands.get('helpmod').run(client, message, args);
+  } else if (command === 'helpfun') {
+  client.commands.get('helpfun').run(client, message, args);
   }
-  if (command === 'h info') {
-  client.commands.get('h info').run(client, message, args);
-  } else if (command === 'h utility') {
-  client.commands.get('h utility').run(client, message, args);
+  if (command === 'helpinfo') {
+  client.commands.get('helpinfo').run(client, message, args);
+  } else if (command === 'helputility') {
+  client.commands.get('helputility').run(client, message, args);
   }
   if (command === 'userid') {
   client.commands.get('userid').run(client, message, args);
@@ -124,10 +124,10 @@ const command = args.shift().toLowerCase();
   } else if (command === "createrole") {
   client.commands.get("createrole").run(client, message, args);
   }
-  if (command === 'h config') {
-  client.commands.get('h config').run(client, message, args);
-  } else if (command === 'h image') {
-  client.commands.get('h image').run(client, message, args);
+  if (command === 'helpconfig') {
+  client.commands.get('helpconfig').run(client, message, args);
+  } else if (command === 'helpimage') {
+  client.commands.get('helpimage').run(client, message, args);
   }
   if (command === 'poll') {
   client.commands.get('poll').run(client, message, args);
