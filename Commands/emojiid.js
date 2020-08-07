a49e55889185a585533c5d279408a83eea54ebcb
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 19:32:26 +0530

    Updated

--- Commands/emojiid.js
+++ Commands/emojiid.js
@@ -4,9 +4,13 @@ module.exports = {
     name: "emojiid",
     description: "Get ID of emojis",
     run: async (client, message, args) => {
-        const emoji = message.guild.emojis.cache.find(r => r.name === args.join(" "));
+        const name = args.join(" ");
+        const emoji = message.guild.emojis.cache.find(r => r.name === name);
+        if (!name) {
+        return message.channel.send("Please type the emoji name')
+        }
         if (!emoji) {
-        return message.channel.send("Please type the name of an emoji");
+        return message.channel.send("Couldn't find the Emojis with the provided name. Please make sure the Emoji name is correct");
         }
         message.channel.send(`\`\`\`${emoji}\`\`\``)
 }
