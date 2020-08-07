4db1cc99f05bd12d3f3d7077a45d101831c08d94
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 16:34:16 +0530

    Update helpconfig.js

--- Commands/helpconfig.js
+++ Commands/helpconfig.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "helpconfig",
+    aliases: ["h config"],
     description: "Get list of Configuration commands",
     run: async(client, message, args) => {
     const embed = new Discord.MessageEmbed()
