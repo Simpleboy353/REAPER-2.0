8d713fc943d716b678d44e9de402e44f0bdb0254
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-07 14:49:50 +0530

    Updated

--- Commands/hack.js
+++ Commands/hack.js
@@ -15,44 +15,44 @@ module.exports = {
     msg.edit(`Finding ${tohack.displayName}'s Email and Password.....`);
   }, ms(time));
 
-    let time1 = '4s'
+    let time1 = '6s'
     setTimeout(function(){
     msg.edit(`E-Mail: ${tohack.displayName}@gmail.com \nPassword: ********`);
   }, ms(time1));
 
-    let time2 = '6s'
+    let time2 = '9s'
     setTimeout(function(){
     msg.edit("Finding Other Accounts.....");
   }, ms(time2));
 
-    let time3 = '8s'
+    let time3 = '15s'
     setTimeout(function(){
     msg.edit("Setting up Epic Games Account.....");
   }, ms(time3));
 
-    let time4 = '10s'
+    let time4 = '21s'
     setTimeout(function(){
     msg.edit("Hacking Epic Games Account......");
   }, ms(time4));
 
-    let time5 = '14s'
+    let time5 = '28s'
     setTimeout(function(){
     msg.edit("Hacked Epic Games Account!!");
   }, ms(time5));
 
-    let time6 = '16s'
+    let time6 = '31s'
     setTimeout(function(){
     msg.edit("Collecting Info.....");
   }, ms(time6));
 
-    let time7 = '20s'
+    let time7 = '38s'
     setTimeout(function(){
     msg.edit("Selling data to FBI....");
   }, ms(time7));
 
-    let time8 = '22s'
+    let time8 = '41s'
     setTimeout(function(){ 
-    msg.edit(`Finished Hacking ${tohack}`);
+    msg.edit(`Finished Hacking ${tohack.displayName}`);
   }, ms(time8));
 
   }
