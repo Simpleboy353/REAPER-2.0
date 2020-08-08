a2cb4b6575337b5c4693d8eed98dcf8193885d7f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-08 17:26:35 +0530

    Updated

--- Commands/greentext.js
+++ Commands/greentext.js
@@ -4,9 +4,10 @@ module.exports = {
     name: "greentext",
     description: "Colors your text with green colour",
     run: async(client, message, args) => {
-    if (!args) {
+    const text = args.join(" ");
+    if (!text) {
     return message.channel.send("You need to enter some text")
     }
-    message.channel.send("```css \nargs.join(" ") \n```")
+    message.channel.send("```css\n{text}\n```")
 }
 }
