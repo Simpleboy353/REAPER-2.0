77889256176a5610c1392f19dae359b0f7eeb840
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-08 17:06:40 +0530

    Another Fun Command Inspired from Dank Memer

---
+++ Commands/greentext.js
@@ -0,0 +1,13 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+    name: "greentext",
+    description: "Colors your text with green colour",
+    run: async(client, message, args) => {
+    const text = args.join(" ");
+    if (!text) {
+    return message.channel.send("You need to enter some text")
+    }
+    message.channel.send(`\`\`\`css \n${text} \n\`\`\``)
+}
+}
