b0cca7d4ac2e41061e9c37612ff13ded0d6fd72a
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-08 12:32:33 +0530

    Updated

--- Commands/giverole.js
+++ Commands/giverole.js
@@ -5,10 +5,12 @@ module.exports = {
     description: "Give roles to users",
     run: async (client, message, args) => {
     if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("You don't have enough permissions");
+        const name = message.mentions.roles.first();
+        if (!name) return message.channel.send("Please mention a role");
         const user = message.mentions.members.first();
         if (!user) return message.channel.send("Please mention a user you want to give the role to");
-        const role = message.guild.roles.cache.find(r => r.name === args.slice(1).join(" "));
-        if (!role) return message.channel.send("Please mention a role");
+        const role = message.guild.roles.cache.find(r => r.name === name);
+        if (!role) return message.channel.send("Couldn't find the Provided Role");
         await user.roles.add(role.id), message.channel.send(`${user} now has the ${role} role`)   
 }
 }
