ece4105d601de5034b07db16fc5a5c4cf5d60aad
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-08 14:21:56 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -3,19 +3,18 @@ const Discord = require("discord.js");
 module.exports = {
     name:"userinfo",
   run: async (bot, message, args) => {
-  var mention = message.mentions.members.first() || message.author;
+  var mention = message.mentions.members.first();
   if(!mention) return message.channel.send("Please mention someone to get their user info.")
   const userlol = new Discord.MessageEmbed()
   .setTitle(`User Info`)
-  .setThumbnail(mention.avatarURL)
+  .setThumbnail(mention.user.avatarURL)
   .setColor("RANDOM")
-  .addField(`**Name: **`, `${mention.user.displayName}`)
-  .addField(`**Tag: **`, `${mention.user.discriminator}`)
-  .addField(`**ID: **`, `${mention.user.id}`)
-  .addField(`**Is Bot: **`, `${mention.user.bot}`)
-  .addField("**Account created at: **", `${mention.user.createdAt}`)
-  .addField("**Joined This Server at: **", `${mention.joinedAt}`)
+  .addField(`Name: `, `**${mention.user.username}**`)
+  .addField(`Tag: `, `**${mention.user.discriminator}**`)
+  .addField(`ID: `, `**${mention.user.id}**`)
+  .addField(`Is Bot: `, `**${mention.user.bot}**`)
+  .addField("Account created at: ", `**${mention.user.createdAt}**`)
+  .addField("Joined This Server at: ", `**${mention.joinedAt}**`)
   message.channel.send(userlol)
-  console.log(`USERINFO command from ${message.author.username} USER ID= ${message.author}`)
 }
 }
