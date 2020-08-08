6fb2d4702746f38b2313216f078ee1b9f7d635bc
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-08 13:50:17 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -3,15 +3,18 @@ const Discord = require("discord.js");
 module.exports = {
     name:"userinfo",
   run: async (bot, message, args) => {
-  var mention = message.guild.member(message.mentions.users.first());
+  var mention = message.mentions.members.first();
   if(!mention) return message.channel.send("Please mention someone to get their user info.")
   const userlol = new Discord.MessageEmbed()
   .setThumbnail(mention.user.avatarURL)
-  .setTitle(`${mention.user.username}\'s Info`)
+  .setTitle(`User Info`)
   .setColor("RANDOM")
-  .addField(`ID`, mention.id)
-  .addField("Account created at", `${mention.user.createdAt}`)
-  .addField("Joined at", `${mention.joinedAt}`)
+  .addField(`**Name: **`, `${mention.username}`)
+  .addField(`**Tag: **`, `${mention.discriminator}`)
+  .addField(`**ID: **`, `${mention.id}`)
+  .addField(`**Is Bot: **`, `${mention.isBot}`)
+  .addField("**Account created at: **", `${mention.user.createdAt}`)
+  .addField("**Joined This Server at: **", `${mention.joinedAt}`)
   message.channel.send(userlol)
   console.log(`USERINFO command from ${message.author.username} USER ID= ${message.author}`)
 }
