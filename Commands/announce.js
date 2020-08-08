2fdcde8af61baf6863471aeeb8ae3ed426dd9f09
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-08 21:31:44 +0530

    Updated for Sending Message in a Mentioned Channel

--- Commands/announce.js
+++ Commands/announce.js
@@ -5,11 +5,15 @@ module.exports = {
     description: "Make an Announcemnet in your Server",
     run: async (clinet, message, args) => {
         const ann = args.join(" ");
+        const anchannel = message.mentions.channels.first();
         if(!message.member.hasPermission("MANAGE_MESSAGES")) {
         return message.channel.send("You don't have enogh Permissions")
         }
         if(!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
         return message.channel.send("I don't have enough Permissions")
+        }
+        if (!anchannel) {
+        return message.channel.send("Mention a channel")
         }
         if (!ann) {
         return message.channel.send("Please add some text to make an Announcement")
@@ -20,7 +24,14 @@ module.exports = {
         .setDescription(`${ann}`)
         .setColor("RANDOM")
         .setFooter(`Announcement by ${message.author.username}`);
-        message.channel.send(embed);
+        anchannel.send(embed);
+    
+        let anembed = new Discord.MessageEmbed))
+        .setTitle("Done!")
+        .setDescription (`Announcement has been sent to ${anchannel}`)
+        .setColor("RANDOM");
+
+        message.channel.send(anembed);
         message.delete();
     }
 }
