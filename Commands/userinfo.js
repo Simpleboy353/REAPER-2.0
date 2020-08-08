07b24a309aa5e6bea22d8b1be0b6a4f141c4faee
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-08 14:08:28 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 module.exports = {
     name:"userinfo",
   run: async (bot, message, args) => {
-  var mention = message.mentions.members.first();
+  var mention = message.mentions.members.first() || message.member();
   if(!mention) return message.channel.send("Please mention someone to get their user info.")
   const userlol = new Discord.MessageEmbed()
-  .setThumbnail(mention.user.avatarURL)
   .setTitle(`User Info`)
+  .setThumbnail(`${mention.user.AvatarURL}`)
   .setColor("RANDOM")
   .addField(`**Name: **`, `${mention.user.username}`)
   .addField(`**Tag: **`, `${mention.user.discriminator}`)
