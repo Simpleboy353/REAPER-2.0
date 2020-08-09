248c3d02d002bd022373c4ec2ae4c7482edba199
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-09 08:27:46 +0530

    Added Fun Command

---
+++ Commands/sacrifice.js
@@ -0,0 +1,46 @@
+const Discord = module.require("discord.js");
+
+var sacrify = [
+    "Cthulu",
+    "The Flying Spaghetti Monster",
+    "the Illuminati",
+    "Ben Brode",
+    "a local school district",
+    "a giant squid",
+    "Herobrine",
+    "the devil",
+    "RNGesusJoe",
+    "Bob",
+    "get the iPhone X",
+    "Santa Claus",
+    "God-Emperor Trump",
+    "cure cancer",
+    "create a new meme",
+    "Nyarlathotep",
+    "Yogg-Saron",
+    "N'Zoth; Y'Shaarj",
+    "C'Thun",
+    "McDonalds",
+    "Dictator Advaith",
+    "Slenderman",
+    "your mom"
+];
+
+
+module.exports = {
+    name: "sacrifice",
+    description: "Sacrifice someone",
+    usage: "sacrifice <thing/person>",
+    category: "fun",
+    run: async (client, message, args) => {
+        
+        const target = message.mentions.members.first() || args.join(" ")
+        
+         if (!target) {
+            return message.channel.send("Please send who or what to sacrifice")
+        }
+        if (target) {
+            return message.channel.send(`${message.author.username} sacrificed **${target.displayName}** to **` + (sacrify[Math.floor(Math.random() * sacrify.length)]) + "**")
+            }
+      }
+}
