c6ad8ab287426bbf0e9f321e2dd4220561f69bf5
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-09 23:05:31 +0530

    Updated

--- Commands/botinfo.js
+++ Commands/botinfo.js
@@ -6,7 +6,6 @@ module.exports = {
     run: async (client, message, args) => {
         let embed = new Discord.MessageEmbed()
         .setAuthor("Infinity#4967's Info", client.user.avatarURL())
-        .setTitle("Bot Info")
         .setColor("RANDOM")
         .setDescription("**Bot Name: **REAPER 2.0 (BETA) \n**Owner: **๖ۣۜℜⱥjͥƤuͣtͫ#5915 \n**Total Categories: **6 \n**Total Commands: **60 \n**State: **Under Development \n**Online Status: **Up 24/7 (Except during Maintenance)")
         .addField("Some Useful Links", "**Get Infinity's Invite Link** **[Here](https://discord.com/api/oauth2/authorize?client_id=733670294086221865&permissions=8&scope=bot)** \n**Looking for a Music Bot? Try Our** **[Beats](https://discord.com/api/oauth2/authorize?client_id=730683152720199700&permissions=37223745&scope=bot) Bot**")
