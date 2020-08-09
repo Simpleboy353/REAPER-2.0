d2909001c1741ffe96c8deee6abbeb79a02444a2
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-09 20:37:14 +0530

    Updated

--- Commands/trash.js
+++ Commands/trash.js
@@ -5,13 +5,13 @@ module.exports = {
    description: "Trash Generator",
    run: async(client, message, args) => {
    const base = message.author;
-   const avatar1 = base.user.displayAvatarURL({ format: 'png', size: 512 });
    const target = message.mentions.members.first();
    if (!target) {
    return message.channel.send("You need to mention Someone")
    }
-   const avatar2 = target.user.displayAvatarURL({ format: 'png', size: 512 });
+   const baseAvatarURL = base.user.displayAvatarURL({ format: 'png', size: 512 });
+   const targetAvatarURL = target.user.displayAvatarURL({ format: 'png', size: 512 });
 
-   message.channel.send({files: [{attachment: `https://api.alexflipnote.dev/trash?face=${avatar1}&trash=${avatar2}`, name: 'file.jpg' }]});
+   message.channel.send({files: [{attachment: `https://api.alexflipnote.dev/trash?face=${baseAvatarURL}&trash=${targetAvatarURL}`, name: 'file.jpg' }]});
 }
 }
