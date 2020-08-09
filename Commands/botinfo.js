4407a98f76f192952c13c048e7c5c1b7362cc79e
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-09 22:45:36 +0530

    Updated

--- Commands/botinfo.js
+++ Commands/botinfo.js
@@ -8,7 +8,8 @@ module.exports = {
         .setTitle("Bot Info")
         .setColor("RANDOM")
         .setDescription("**Bot Name: **REAPER 2.0 (BETA) \n**Owner: **๖ۣۜℜⱥjͥƤuͣtͫ#5915 \n**Total Categories: **6 \n**Total Commands: **60 \n**State: **Under Development \n**Online Status: **Up 24/7 (Except during Maintenance)")
-        .setFooter("Thanks, Reaper 2.0 Team");
-        message.channel.send(embed);
+        .addField("Some Useful Links", "**Get Infinity's Invite Link **[Here](https://discord.com/api/oauth2/authorize?client_id=733670294086221865&permissions=8&scope=bot) \n**Looking for a Music Bot? Try Ours **[Beats](https://discord.com/api/oauth2/authorize?client_id=730683152720199700&permissions=37223745&scope=bot)")
+        .setFooter("Regards, Infinity Bot Team");
+    message.channel.send(embed);
     }
 }
