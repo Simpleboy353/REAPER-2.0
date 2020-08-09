078cdef55fe1fa53bc71c3a15bb4a9ea3708b976
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-09 23:03:53 +0530

    Updated

--- Commands/botinfo.js
+++ Commands/botinfo.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "Shows the bot info",
     run: async (client, message, args) => {
         let embed = new Discord.MessageEmbed()
-        .setAuthor("Infinity#4967's Info", client.avatarURL())
+        .setAuthor("Infinity#4967's Info", client.user.avatarURL())
         .setTitle("Bot Info")
         .setColor("RANDOM")
         .setDescription("**Bot Name: **REAPER 2.0 (BETA) \n**Owner: **๖ۣۜℜⱥjͥƤuͣtͫ#5915 \n**Total Categories: **6 \n**Total Commands: **60 \n**State: **Under Development \n**Online Status: **Up 24/7 (Except during Maintenance)")
