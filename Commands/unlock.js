b155e7654306610c5f9e1abd77f3f2b81386910b
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-10 22:05:05 +0530

    Updated

---
+++ Commands/unlock.js
@@ -0,0 +1,23 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+   name: "unlock",
+   description: "Unlocks a Channel",
+   run: async(client, message, args) => {
+   if (!message.member.hasPermission('MANAGE_SERVER', 'MANAGE_CHANNELS')) {
+   return message.channel.send("You don't have enough Permissions")
+   }
+   message.channel.overwritePermissions([
+     {
+        id: message.guild.id,
+        allow : ['SEND_MESSAGES'],
+     },
+    ],);
+   const embed = new Discord.MessageEmbed()
+   .setTitle("Channel Updates")
+   .setDescription("🔓 This Channel has been Unlocked")
+   .setColor("RANDOM");
+   await message.channel.send(embed);
+   message.delete();
+}
+}
