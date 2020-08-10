794c71ab701a6a084af6ce1ea921c1c6b545907d
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-10 22:32:15 +0530

    Updated

--- Commands/botinfo.js
+++ Commands/botinfo.js
@@ -7,7 +7,7 @@ module.exports = {
         let embed = new Discord.MessageEmbed()
         .setAuthor("Infinity#4967's Info", client.user.avatarURL())
         .setColor("RANDOM")
-        .setDescription("**Bot Name: **Infinity \n**Owner: **๖ۣۜℜⱥjͥƤuͣtͫ#5915 \n**Total Categories: **6 \n**Total Commands: **60 \n**State: **Under Development \n**Online Status: **Up 24/7 (Except during Maintenance)")
+        .setDescription("**Bot Name: **Infinity \n**Owner: **๖ۣۜℜⱥjͥƤuͣtͫ#5915 \n**Total Categories: **6 \n**Total Commands: **64 \n**State: **Under Development \n**Online Status: **Up 24/7 (Except during Maintenance)")
         .addField("Some Useful Links", "**Get Infinity's Invite Link** **[Here](https://discord.com/api/oauth2/authorize?client_id=733670294086221865&permissions=8&scope=bot)** \n**Looking for a Music Bot? Try Our** **[Beats](https://discord.com/api/oauth2/authorize?client_id=730683152720199700&permissions=37223745&scope=bot) Bot**")
         .setFooter("Regards, Infinity Bot Team");
     message.channel.send(embed);
