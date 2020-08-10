21e24fad98f3af3735be8067c8a5dee72b4b2801
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-10 22:19:41 +0530

    Updated

---
+++ Commands/delrole.js
@@ -0,0 +1,21 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+    name: "delrole",
+    description: "Deletes a role",
+    run: async(client, message, args) => {
+    const role = message.mentions.roles.first();
+    if (!message.member.hasPermission("MANAGE_ROLES")) {
+    return message.channel.send("You don't have enough Permissions")
+    }
+    if (!role) {
+    return message.channel.send("You need to mention a role")
+    }
+    message.guild.roles.delete(role);
+    const embed = new Discord.MessageEmbed()
+    .setTitle("Roles Update")
+    .setDescription (`${role} role has been deleted`)
+    .setColor("RANDOM");
+  await message.channel.send(embed);
+}
+}
