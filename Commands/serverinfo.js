8e88368360d153579bf2ff25694a6d7337edd390
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-10 16:46:22 +0530

    Updated

--- Commands/serverinfo.js
+++ Commands/serverinfo.js
@@ -9,12 +9,33 @@ run: async (client, message, args) => {
 //command
 let servericon = message.guild.iconURL;
 let serverembed = new Discord.MessageEmbed()
+let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+let region = {
+        "brazil": ":flag_br: Brazil",
+        "eu-central": ":flag_eu: Central Europe",
+        "singapore": ":flag_sg: Singapore",
+        "us-central": ":flag_us: U.S. Central",
+        "sydney": ":flag_au: Sydney",
+        "us-east": ":flag_us: U.S. East",
+        "us-south": ":flag_us: U.S. South",
+        "us-west": ":flag_us: U.S. West",
+        "eu-west": ":flag_eu: Western Europe",
+        "vip-us-east": ":flag_us: VIP U.S. East",
+        "london": ":flag_gb: London",
+        "amsterdam": ":flag_nl: Amsterdam",
+        "hongkong": ":flag_hk: Hong Kong",
+        "russia": ":flag_ru: Russia",
+        "southafrica": ":flag_za:  South Africa",
+        "india": "🇮🇳: India"
+    };
 .setTitle("Server Information")
 .setColor("RANDOM")
 .setThumbnail(servericon)
 .addField("Server Name", message.guild.name)
 .addField("Owner", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, true)
+.addField("Region", region[message.guild.region], true)
 .addField("Channels", message.guild.channels.cache.size, true)
+.addField("Verification Level", verifLevels[message.guild.verificationLevel], true)
 .addField("Roles", message.guild.roles.cache.size, true)
 .addField("Created On", message.guild.createdAt)
 .addField("You Joined", message.member.joinedAt)
