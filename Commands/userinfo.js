fedfa1108b76fd5f2cd7aca65a7a5f93409f7736
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-10 17:45:06 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -5,14 +5,19 @@ module.exports = {
   run: async (bot, message, args) => {
   var mention = message.mentions.members.first();
   if(!mention) return message.channel.send("Please mention someone to get their user info.")
+  let usericon = mention.user.avatarURL;
+  let bot = {
+      "true": "The User is a Bot",
+      "false": "The User is a Human"
+    };
   const userlol = new Discord.MessageEmbed()
   .setTitle(`User Info`)
-  .setThumbnail(mention.user.avatarURL)
+  .setThumbnail(usericon)
   .setColor("RANDOM")
   .addField(`Name: `, `**${mention.user.username}**`)
   .addField(`Tag: `, `**#${mention.user.discriminator}**`)
   .addField(`ID: `, `**${mention.user.id}**`)
-  .addField(`Is Bot: `, `**${mention.user.bot}**`)
+  .addField(`Is Bot: `, `**bot[${mention.user.bot}]**`)
   .addField("Account created at: ", `**${mention.user.createdAt}**`)
   .addField("Joined This Server at: ", `**${mention.joinedAt}**`)
   message.channel.send(userlol)
