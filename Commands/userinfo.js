15e799c3dd2c22d270d7038891d4a0e49c29b95f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-11 16:23:46 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -4,16 +4,17 @@ module.exports = {
     name:"userinfo",
   run: async (client, message, args) => {
   var member = message.mentions.members.first() || message.author;
+  user = member.user;
   if(!member) return message.channel.send("Please mention someone to get their user info.")
   const userlol = new Discord.MessageEmbed()
   .setTitle(`User Info`)
   .setColor("RANDOM")
-  .addField(`Name: `, `**${member.username}**`)
-  .addField(`Tag: `, `**#${member.discriminator}**`)
-  .addField(`ID: `, `**${member.id}**`)
-  .addField(`Is Bot: `, `**${member.bot}**`)
-  .addField(`Roles: `,  member.roles.map(roles => `${roles}`).join(', '))
-  .addField("Account created at: ", `**${member.createdAt}**`)
+  .addField(`Name: `, `**${member..user.username}**`)
+  .addField(`Tag: `, `**#${member.user.discriminator}**`)
+  .addField(`ID: `, `**${member.user.id}**`)
+  .addField(`Is Bot: `, `**${member.user.bot}**`)
+  .addField(`Roles: `,  user.roles ? user.roles.map(roles => `${roles}`).join(', '))
+  .addField("Account created at: ", `**${member.user.createdAt}**`)
   .addField("Joined This Server at: ", `**${member.joinedAt}**`)
 
   message.channel.send(userlol)
