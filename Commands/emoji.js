8df78e83d6397e699aeca0a839e4608a5c3aa3d4
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-11 22:46:18 +0530

    Added a Utility Command

---
+++ Commands/emoji.js
@@ -0,0 +1,17 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+    name: "emojiid",
+    description: "Get ID of emojis",
+    run: async (client, message, args) => {
+        const name = args.join(" ");
+        const emoji = message.guild.emojis.cache.find(r => r.name === name);
+        if (!name) {
+        return message.channel.send("Please type the emoji name")
+        }
+        if (!emoji) {
+        return message.channel.send("Couldn't find the Emojis with the provided name. Please make sure the Emoji name is correct");
+        }
+        message.channel.send(`${emoji}`)
+}
+}
