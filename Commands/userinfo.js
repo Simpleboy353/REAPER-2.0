670dafd284a097e5e7bd1b50168f29069a0390fe
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-11 16:11:55 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -5,10 +5,8 @@ module.exports = {
   run: async (client, message, args) => {
   var member = message.mentions.members.first() || message.author;
   if(!member) return message.channel.send("Please mention someone to get their user info.")
-  let usericon = member.user.avatarURL;
   const userlol = new Discord.MessageEmbed()
   .setTitle(`User Info`)
-  .setThumbnail(usericon)
   .setColor("RANDOM")
   .addField(`Name: `, `**${member.user.username}**`)
   .addField(`Tag: `, `**#${member.user.discriminator}**`)
@@ -17,7 +15,7 @@ module.exports = {
   .addField(`Roles: `,  member.roles.map(roles => `${roles}`).join(', '))
   .addField("Account created at: ", `**${member.user.createdAt}**`)
   .addField("Joined This Server at: ", `**${member.joinedAt}**`)
-  .setThumbnail(member.user.avatarURL())
+
   message.channel.send(userlol)
 }
 }
