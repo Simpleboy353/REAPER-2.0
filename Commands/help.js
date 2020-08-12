692cc8e92f2e08b7672da0257cd16546e7c9327c
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-08-12 18:08:55 +0530

    Updated

--- Commands/help.js
+++ Commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
         }
 
         let prefix = prefixes[message.guild.id].prefix; //Let prefix be prefixes[msg.guild.id].prefix
-    const link = `https://camo.githubusercontent.com/2bc9b70767975b9f620681eca883a7978fa9a354/68747470733a2f2f692e696d6775722e636f6d2f355245665562482e706e67`;
+
     const embed = new Discord.MessageEmbed()
 .setTitle('Welcome to the Help Section \nPrefix for your Server: '+prefix)
 .addField(`⚙ __**Configuration**__: `, "`=helpconfig`", true)
@@ -23,7 +23,6 @@ module.exports = {
 .addField(`ℹ __**Info**__: `, "`=helpinfo`", true)
 .addField(`🛡 __**Moderation**__: `, "`=helpmod`", true)
 .addField(`🛠 __**Utility**__: `, "`=helputility`", true)
-.setImage(link)
 .setColor("RANDOM");
 
 message.channel.send(embed);
