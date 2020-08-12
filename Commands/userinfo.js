2b45b8d98d82fca5516d568378d18a132ead49c9
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-12 08:09:25 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -3,8 +3,8 @@ const Discord = module.require("discord.js");
 module.exports = {
     name:"userinfo",
   run: async (client, message, args) => {
-  const mention = message.mentions.members.last() || message.mention;
-  const usericon = mention.user.avatarURL();
+  const mention = message.mentions.members.first();
+  const usericon = mention.user.displayAvatarURL({dynamic: true, size: 512});
   const userlol = new Discord.MessageEmbed()
   .setTitle(`User Info`)
   .setColor("RANDOM")
@@ -15,7 +15,7 @@ module.exports = {
   .addField(`Is Bot: `, `**${mention.user.bot}**`)
   .addField("Account created On: ", `**${mention.user.createdAt}**`)
   .addField("Joined This Server On: ", `**${mention.joinedAt}**`)
-  .setThumbnail(mention.user.avatarURL())
+  .setThumbnail(`mention.user.displayAvatarURL({dynamic: true, size: 512})
   message.channel.send(userlol)
 }
 }
