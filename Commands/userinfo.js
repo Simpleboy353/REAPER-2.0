6add9c06a74a931e3bdb5b89febbb1e2c066a5a0
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-12 08:46:52 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -8,6 +8,20 @@ module.exports = {
   return message.channel.send("Please Mention Someone")
   }
   const usericon = mention.user.displayAvatarURL({dynamic: true, size: 512});
+  var flags = {
+   "DISCORD_EMPLOYEE": "Discord Employee",
+   "DISCORD_PARTNER": "Discord Partner",
+   "BUGHUNTER_LEVEL_1": "Bug Hunter (Level 1)",
+   "BUGHUNTER_LEVEL_2": "Bug Hunter (Level 2)",
+   "HYPESQUAD_EVENTS": "Hypesquad Events",
+   "HOUSE_BRILLIANCE": "House of Brilliance",
+   "HOUSE_BRAVERY": "House of Bravery",
+   "HOUSE_BALANCE": "House of Balance",
+   "EARLY_SUPPORTER": "Early Supporter",
+   "TEAM_USER": "Team User",
+   "VERIFIED_BOT": "Verified Bot",
+   "VERIFIED_DEVELOPER": "Verified Bot Developer"
+              };
   var bot = {
      "true": "Yes, The User is a Bot",
      "false": "No, The User is a Human"
@@ -20,7 +34,7 @@ module.exports = {
   .addField(`Tag: `, `#${mention.user.discriminator}`)
   .addField(`ID: `, `${mention.user.id}`)
   .addField(`Is Bot: `, bot[mention.user.bot])
-  .addField(`Flags: `, `${mention.user.flags}`)
+  .addField(`Flags: `, flags[mention.user.flags.toArray()])
   .addField("Account created On: ", `${mention.user.createdAt}`)
   .addField("Joined This Server On: ", `${mention.joinedAt}`)
   .setThumbnail(mention.user.displayAvatarURL({dynamic: true, size: 512}))
