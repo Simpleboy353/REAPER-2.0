d3670054c88dae57f1f30e8775bf4874343a01ce
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-12 08:02:12 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -5,10 +5,7 @@ module.exports = {
   run: async (client, message, args) => {
   const mention = message.mentions.members.last() || message.mention;
   const roles = mention.roles.cache
-  .sort((a, b) => b.postion - a.position)
-  .map(role => role.toString())
-  .slice(0, -1);
-  const usericon = mention.user.avatarURL;
+  const usericon = mention.user.displayAvatarURL();
   const userlol = new Discord.MessageEmbed()
   .setTitle(`User Info`)
   .setColor("RANDOM")
@@ -17,7 +14,6 @@ module.exports = {
   .addField(`Tag: `, `**#${mention.user.discriminator}**`)
   .addField(`ID: `, `**${mention.user.id}**`)
   .addField(`Is Bot: `, `**${mention.user.bot}**`)
-  .addField(`Roles [${roles.length}]: `, `${roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : None}`)
   .addField("Account created On: ", `**${mention.user.createdAt}**`)
   .addField("Joined This Server On: ", `**${mention.joinedAt}**`)
   .setThumbnail(mention.user.avatarURL())
