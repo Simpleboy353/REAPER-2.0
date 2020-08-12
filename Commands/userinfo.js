f31747b97884660c775e2ffc14167725525ab6c8
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-12 07:44:25 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -2,9 +2,12 @@ const Discord = module.require("discord.js");
 
 module.exports = {
     name:"userinfo",
-  run: async (client, message, args) => {
-  var mention = message.mentions.members.first();
-  if(!mention) return message.channel.send("Please mention someone to get their user info.")
+  run: async (client, message, [target]) => {
+  const mention = message.mentions.members.last() || message.guild cache.get(target) || message.member;
+  const roles = member.roles cache
+  .sort((a, b) => b.postion - a.position)
+  .map(role => role.toString())
+  .slice(0, -1);
   const usericon = mention.user.avatarURL;
   const userlol = new Discord.MessageEmbed()
   .setTitle(`User Info`)
@@ -14,6 +17,7 @@ module.exports = {
   .addField(`Tag: `, `**#${mention.user.discriminator}**`)
   .addField(`ID: `, `**${mention.user.id}**`)
   .addField(`Is Bot: `, `**${mention.user.bot}**`)
+  .addField(`Roles [${roles.length}]: `, `${roles.length < 10 ? roles.join(', ') : roles.length > 10 ? this.client.utils.trimArray(roles) : None`)
   .addField("Account created On: ", `**${mention.user.createdAt}**`)
   .addField("Joined This Server On: ", `**${mention.joinedAt}**`)
   .setThumbnail(mention.user.avatarURL())
