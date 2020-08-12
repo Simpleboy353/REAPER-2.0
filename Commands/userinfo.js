45b83f115f96589f78a477ac58a1444f2c7c5ea0
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-12 08:15:26 +0530

    Updated

--- Commands/userinfo.js
+++ Commands/userinfo.js
@@ -5,17 +5,21 @@ module.exports = {
   run: async (client, message, args) => {
   const mention = message.mentions.members.first();
   const usericon = mention.user.displayAvatarURL({dynamic: true, size: 512});
+  var bot = [
+     "true": "Yes, The User is a Bot",
+     "false": "No, The User is a Human"
+        ];
   const userlol = new Discord.MessageEmbed()
   .setTitle(`User Info`)
   .setColor("RANDOM")
   .setThumbnail(usericon)
-  .addField(`Name: `, `**${mention.user.username}**`)
-  .addField(`Tag: `, `**#${mention.user.discriminator}**`)
-  .addField(`ID: `, `**${mention.user.id}**`)
-  .addField(`Is Bot: `, `**${mention.user.bot}**`)
-  .addField("Account created On: ", `**${mention.user.createdAt}**`)
-  .addField("Joined This Server On: ", `**${mention.joinedAt}**`)
-  .setThumbnail(mention.user.displayAvatarURL({dynamic: true, size: 512})
+  .addField(`Name: `, `${mention.user.username}`)
+  .addField(`Tag: `, `#${mention.user.discriminator}`)
+  .addField(`ID: `, `${mention.user.id}`)
+  .addField(`Is Bot: `, bot[mention.user.bot])
+  .addField("Account created On: ", `${mention.user.createdAt}`)
+  .addField("Joined This Server On: ", `${mention.joinedAt}`)
+  .setThumbnail(mention.user.displayAvatarURL({dynamic: true, size: 512}))
   message.channel.send(userlol)
 }
 }
