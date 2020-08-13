8f6076becb1381e56b265fed64de7f028992b55e
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-08-13 20:12:07 +0530

    Updated

--- index.js
+++ index.js
@@ -18,12 +18,13 @@ client.on('ready', () => {
   const activities_list = [
    "=help", 
    "Infinity Rocks",
-   "Running 24/7", 
+   "Running 24/7",
+   "=help",
    "Helping You make your Server Better"
     ]; // creates an arraylist containing phrases you want your bot to switch through.
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        client.user.setActivity(activities_list[index], {type : "STREAMING"}); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.});
 });
 
