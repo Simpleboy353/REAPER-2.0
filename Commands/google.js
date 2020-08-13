55d33c1f654ed0672faf1101134a1ddd0b1cdf50
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-08-13 18:07:47 +0530

    Updated

--- Commands/google.js
+++ Commands/google.js
@@ -6,13 +6,13 @@ module.exports = {
     run: async(client, message, args) => {
     const text1 = args.join(' ');
     const text2 = args.join('+');
-    const google = `https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg`;
+    const google = `https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2000px-Google_%22G%22_Logo.svg.png`;
     if (!text2) {
     return message.channel.send("Enter some to search for")
     }
     const embed = new Discord.MessageEmbed()
-    .setAuthor("Google", `https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg`)
-    .setThumbnail(`https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg`)
+    .setAuthor("Google", `https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2000px-Google_%22G%22_Logo.svg.png`)
+    .setThumbnail(`https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2000px-Google_%22G%22_Logo.svg.png`)
     .setDescription(`**Searched for: **\n${text1} \n\n**Result: **\n[Here's What I found](https://google.com/search?q=${text2})`)
     .setThumbnail(google)
     .setColor("RANDOM");
