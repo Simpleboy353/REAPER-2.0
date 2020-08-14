ee264673fd1817d49b925cb4e43a89d6fc225960
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-08-14 20:14:50 +0530

    Updated

--- Commands/avatar.js
+++ Commands/avatar.js
@@ -11,7 +11,7 @@ module.exports = {
 let user = message.mentions.users.first() || message.author;
 let embed = new discord.MessageEmbed()
         .setColor("RANDOM")
-        .addField(`${user.username}'s Avatar`, `[Avatar Link](user.avatarURL({size: 2048, dynamic: true, format: "png"}))`)
+        .addField(`${user.username}'s Avatar`, `[Avatar Link](${user.displayAvatarURL})`)
         .setImage(user.avatarURL({size: 2048, dynamic: true, format: "png"}))
         
     message.channel.send(embed)
