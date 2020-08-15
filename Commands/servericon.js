18c201efafe180fe050e88f44b62b929771b60c7
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-08-15 14:53:41 +0530

    Updated

--- Commands/servericon.js
+++ Commands/servericon.js
@@ -9,7 +9,5 @@ module.exports = {
     .setImage(message.guild.displayIconURL({size: 2048, dynamic: true, format: "png"}))
     .setColor("RANDOM");
  message.channel.send(embed);
-} catch(e) {
-message.channel.send("Error on Server Icon Command. \nError: "+e);
 }
 }
