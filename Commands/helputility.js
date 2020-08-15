e75e8bb61f302e3fef7f6be79feea0a445011f53
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-08-15 15:06:56 +0530

    Updated

--- Commands/helputility.js
+++ Commands/helputility.js
@@ -6,7 +6,7 @@ module.exports = {
     run: async (client, message, args) => {
         const embed = new Discord.MessageEmbed()
         .setTitle("Utility Commands")
-        .setDescription("`avatar`, `announce`, `clear`, `createrole`, `delchannel`, `delrole`, `emoji`, `giverole`, `google`, `lock`, `newtext`, `newvoice`, `nickname`, `poll`, `removerole`, `say`, `unlock`, `weather`")
+        .setDescription("`avatar`, `announce`, `clear`, `createrole`, `delchannel`, `delrole`, `emoji`, `giverole`, `google`, `lock`, `newtext`, `newvoice`, `nickname`, `poll`, `removerole`, `say`, `servericon`, `unlock`, `weather`")
         .setColor("RANDOM");
 
         message.channel.send(embed);
