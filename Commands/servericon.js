96a128c9ecaccab27d1362e2c54a68aeb725f1ac
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-08-15 14:58:53 +0530

    Updated

--- Commands/servericon.js
+++ Commands/servericon.js
@@ -7,8 +7,10 @@ module.exports = {
     const server = message.guild;
     const embed = new Discord.MessageEmbed()
     .setTitle(`${message.guild.name}'s Icon`)
+    .setDescription(`[Icon Link](${server.iconURL({size: 2048, dynamic: true, format: "png"}))`)
     .setImage(server.iconURL({size: 2048, dynamic: true, format: "png"}))
     .setColor("RANDOM");
  message.channel.send(embed);
+message.delete();
 }
 }
