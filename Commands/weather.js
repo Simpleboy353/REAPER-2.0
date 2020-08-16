87319470767d4c8770cdeab7a2286fb1ce289575
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-08-16 18:29:24 +0530

    Updated

--- Commands/weather.js
+++ Commands/weather.js
@@ -4,13 +4,14 @@ module.exports = {
    name: "weather",
    description: "Shows Weather for the provided place",
    run: async(client, message, args) => {
-   const place = args.join(" ");
+   const name = args.join(" ");
+   const place = args.join("-");
    if (!place) {
    return message.channel.send("Please enter the name of a Country/City/Town")
    }
    const link = `https://wttr.in/${place}`
    const embed = new Discord.MessageEmbed()
-   .setTitle(`${place}'s Weather for Next 3 days`)
+   .setTitle(`${name}'s Weather for Next 3 days`)
    .setImage(link)
    .setFooter(`Credits to wttr.in`)
    .setColor("RANDOM");
