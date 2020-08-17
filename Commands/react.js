b7a5e5e2e63dde1e84bebdaa62c4a067b02f4312
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-17 19:24:08 +0530

    Updated

--- Commands/react.js
+++ Commands/react.js
@@ -7,7 +7,7 @@ module.exports = {
     const { text } = args;
     const m = args.message;
 
-    for (const c of text.isLowerCase()) {
+    for (const m of text.toLowerCase()) {
         if (c in map) {
         try {
             await m.react(map[c])
