6cc7ec817d9d018b3f980800054312fc27323a1b
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-18 20:06:01 +0530

    Fixed Errors

--- Commands/partnerships.js
+++ Commands/partnerships.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "partnerships",
     description: "Shows Info Relating Partner Servers and Bots",
     run: async(client, message, args) => {
-        const embed = new Discord.MesaageEmbed()
+        const embed = new Discord.MessageEmbed()
         .setTitle("Infinity Partners")
         .addField(`**__Partner Servers__**`, `**__[/r/Champions](https://discord.gg/UnEU6Pc)__**\n**__[Infinity's Infinite Loop](https://discord.gg/X9PAszD)__**`)
         .addField(`**__Partner Bots__**`, `**__[Beats](https://discord.com/oauth2/authorize?client_id=730683152720199700&permissions=37223745&scope=bot)__**`)
