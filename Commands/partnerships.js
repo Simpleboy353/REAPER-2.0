8e0afc798463588169dc810c14a4cafdb5ddf01b
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-19 10:10:48 +0530

    Updated

--- Commands/partnerships.js
+++ Commands/partnerships.js
@@ -8,7 +8,7 @@ module.exports = {
         .setTitle("Infinity Partners")
         .addField(`**__Partner Servers__**`, `**__[/r/Champions](https://discord.gg/UnEU6Pc)__**\n**__[Infinity's Infinite Loop](https://discord.gg/X9PAszD)__**`)
         .addField(`**__Partner Bots__**`, `**__[Beats](https://discord.com/oauth2/authorize?client_id=730683152720199700&permissions=37223745&scope=bot)__**`)
-        .setFooter(`Contact __**๖ۣۜℜⱥjͥƤuͣtͫ_#5915**__ to become a partner`)
+        .setFooter(`Contact ๖ۣۜℜⱥjͥƤuͣtͫ#5915 to become a partner`)
         .setColor("RANDOM");
     message.channel.send(embed);
     }
