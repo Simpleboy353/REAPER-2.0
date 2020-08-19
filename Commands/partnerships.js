e6a214d0c2d3876fb2f759cdc075175d67a73a4b
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-19 10:32:17 +0530

    Fixed Errors

--- Commands/partnerships.js
+++ Commands/partnerships.js
@@ -4,14 +4,14 @@ module.exports = {
     name: "partnerships",
     description: "Shows Info Relating Partner Servers and Bots",
     run: async(client, message, args) => {
-        const avatar = client.avatarURL;
+        const avatar = client.user.avatarURL;
         const embed = new Discord.MessageEmbed()
         .setThumbnail(avatar)
         .setTitle("Infinity Partners")
         .addField(`**__Partner Servers__**`, `**__[/r/Champions](https://discord.gg/UnEU6Pc)__**\n**__[Infinity's Infinite Loop](https://discord.gg/X9PAszD)__**`)
         .addField(`**__Partner Bots__**`, `**__[Beats](https://discord.com/oauth2/authorize?client_id=730683152720199700&permissions=37223745&scope=bot)__**`)
         .setFooter(`Contact ๖ۣۜℜⱥjͥƤuͣtͫ#5915 to become a partner`)
-        .setThumbnail(client.avatarURL())
+        .setThumbnail(client.user.avatarURL())
         .setColor("RANDOM");
     message.channel.send(embed);
     }
