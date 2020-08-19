1e3afc813366013938ca04e597efa91c84dc18a0
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-19 18:47:41 +0530

    Updated

--- index.js
+++ index.js
@@ -270,6 +270,9 @@ const command = args.shift().toLowerCase();
   } else if (command === 'timedlockdown') {
   client.commands.get('timedlockdown').run(client, message, args);
   }
+  if (command === 'react') {
+  client.commands.get('react').run(client, message, args);
+  }
 });
 
 client.login(process.env.token);
