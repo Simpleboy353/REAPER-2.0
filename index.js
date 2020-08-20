87b7ffd95dd438da03ea4ca0e8e69f48dfb53eff
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-20 17:55:22 +0530

    Updated

--- index.js
+++ index.js
@@ -16,16 +16,18 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
    client.user.setPresence({ status: 'online' });
   const activities_list = [ 
-   `in ${client.guilds.cache.size} Servers`,
-   "Infinity Rocks",
-   "Your Server 24/7",
-   "=help",
-   "Helping You make your Server Better",
-   `in ${client.guilds.cache.size} Servers`
+   { msg: `in ${client.guilds.cache.size} Servers`, type:"PLAYING"},
+   { msg: "Infinity Rocks", type:"STREAMING"},
+   { msg: "Over Your Server 24/7", type:"WATCHING"},
+   { msg: "=help", type:"LISTENING"},
+   { msg: "Helping You make your Server Better", type:"PLAYING"},
+   { msg: `in ${client.guilds.cache.size} Servers`, type:"PLAYING"},
     ];// creates an arraylist containing phrases you want your bot to switch through.
    setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5). 
-        client.user.setActivity(activities_list[index], {type:"STREAMING"}); // sets bot's activities to one of the phrases in the arraylist.
+        client.user.setActivity(activities_list[index].msg, {
+          type: activities_list[index].type,
+        }); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.});
 });
 
