7c66772f3cfd0b781cdb07368ccba723c068360a
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-21 16:15:24 +0530

    Updated

--- Commands/play.js
+++ Commands/play.js
@@ -10,7 +10,7 @@ module.exports = {
   cooldown: 3,
   aliases: ["p"],
   description: "Plays audio from YouTube or Soundcloud",
-   execute(message, args) {
+   async execute(message, args) {
     const { channel } = message.member.voice;
 
     const serverQueue = message.client.queue.get(message.guild.id);
