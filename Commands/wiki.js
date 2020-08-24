176164123a0a74f5b4eb9c5da5c8800bc0f3b5c9
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-24 22:46:30 +0530

    Updated

--- Commands/wiki.js
+++ Commands/wiki.js
@@ -6,6 +6,9 @@ module.exports = {
     run: async(client, message, args) => {
         const search = args.join("_");
         const msg = args.join(" ");
+        if (!msg) {
+            return message.channel.send("You need to enter some text to search for")
+        }
         const link = `https://www.wikipedia.org/w/index.php?search=${search}&ns0=1`;
         const embed = new Discord.MessageEmbed()
         .setTitle("Wikipedia Search")
