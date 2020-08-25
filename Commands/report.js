db25480cd6a5b0a1750984c125c0d60988382ed0
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-25 11:50:46 +0530

    Updated

--- Commands/report.js
+++ Commands/report.js
@@ -9,9 +9,10 @@ module.exports = {
         if (!report) {
             return message.channel.send("Enter the Description of the bug you encountered!");
         }
+        message.channel.send(`${message.author}, Your Report has been Successfully Submitted. Our Mod Team will reply to you as soon as possible`)
         const embed = new Discord.MessageEmbed()
         .setTitle("New Bug Report")
-        .setDescription(`${report} \nReport by: ${message.author.tag}`)
+        .setDescription(`${report} \n\nBy: ${message.author.tag}`)
         .setFooter(`User ID: ${message.author.id}`)
         .setColor("RANDOM");
 
