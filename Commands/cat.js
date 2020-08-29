0e356fc1c5b0e859273ec3a2b744b2b1c5a25e5f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-29 14:02:00 +0530

    Updated

--- Commands/cat.js
+++ Commands/cat.js
@@ -6,6 +6,6 @@ module.exports = {
     run: async(client, message, args) => {
     const ino = Math.random(1,122);
 
-    message.channel.send({files: [{attachment: `hhtps://api.hyrousek.tk/cat`, name: "cat.jpg"}]});
+    message.channel.send({files: [{attachment: `https://api.hyrousek.tk/cat`, name: "cat.jpg"}]});
     }
 }
