d5dbeb057479d8a940bdcb8e0b46efa63ef253bd
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-29 13:53:34 +0530

    Updated

--- Commands/cat.js
+++ Commands/cat.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "Get Random cat images",
     run: async(client, message, args) => {
     const ino = Math.random(1,122);
-    const link = `https://api.hyrousek.tk/cat/${ino}.png`
+    const link = `https://api.hyrousek.tk/cat/${ino}`;
     const embed = new Discord.MessageEmbed()
     .setTitle("Cats")
     .setImage(link)
