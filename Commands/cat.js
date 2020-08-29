d3efd7e03efcdae29ba40414fbbc45da37d3bcce
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-08-29 14:11:37 +0530

    Updated

--- Commands/cat.js
+++ Commands/cat.js
@@ -4,6 +4,10 @@ module.exports = {
     name: "cat",
     description: "Get Random cat images",
     run: async(client, message, args) => {
+    const state = "disabled";
+    if (state === "disabled") {
+    message.channel.send("The command has been disabled for now!")
+    }
     const ino = Math.random(1,122);
 
     message.channel.send({files: [{attachment: `https://api.hyrousek.tk/fox`, name: "cat.jpg"}]});
