7a68c509b7404165a31d000e596e2123877f1ba7
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-09-01 15:52:45 +0530

    Updated

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.on('ready', () => {
     ];// creates an arraylist containing phrases you want your bot to switch through.
    setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5). 
-        client.user.setActivity(activities_list[index].msg, {
+      client.user.setActivity(activities_list[index].msg, {
           type: activities_list[index].type,
         }); // sets bot's activities to one of the phrases in the arraylist.
     }, 10000); // Runs this every 10 seconds.});
@@ -43,9 +43,9 @@ client.on('message', message => {
     .addField(`<:mod:748544387499294841> __**Moderation**__: `, "`=helpmod`\nIssues relating some users? Use these Commands")
     .addField(`<:utility:748177830134808597> __**Utility**__: `, "`=helputility`\nSome Simple Utility Commands")
     .setColor("RANDOM");
-
+ 
   const pinged = message.mentions.members.first();
-  if (pinged.id === "733670294086221865") {
+  if (message.content === `${pinged}&&${pinged.id} === "733670294086221865"`) {
     message.channel.send(embed);
   } else {
     return;
