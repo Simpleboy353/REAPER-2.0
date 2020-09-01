aa053464b47407bb1e0da2b60f20efc200865a94
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-09-01 11:57:21 +0530

    Updated

--- Commands/partnerships.js
+++ Commands/partnerships.js
@@ -4,7 +4,7 @@ module.exports = {
     name: "partnerships",
     description: "Shows Info Relating Partner Servers and Bots",
     run: async(client, message, args) => {
-        const avatar = client.user.avatarURL();
+        const avatar = client.user.displayAvatarURL();
         const embed = new Discord.MessageEmbed()
         .setThumbnail(avatar)
         .setTitle("Infinity Partners")
@@ -15,8 +15,8 @@ module.exports = {
         .setColor("RANDOM");
     message.channel.send(embed);
     }, catch(error) {
-        const errorlogs = client.channels.cache.get("747750993583669258");
+        const errorlogs = client.channels.cache.get("747750993583669258")
         message.channel.send("Looks Like an Error has Ocurred. The Error has been reported to the Report Section!")
-        errorlogs.send("Error on Partnerships Command \nError: \n" + error)
+        errorlogs.send("Error on Partnerships Command \nError: \n" + error);
     }
 }
