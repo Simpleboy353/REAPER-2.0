929ba3aa3574026b72f7e32e2e787df8bc8a133c
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-09-01 11:39:46 +0530

    Updated

--- Commands/nitro.js
+++ Commands/nitro.js
@@ -15,11 +15,6 @@ module.exports = {
    `https://cdn.discordapp.com/attachments/716917641209708647/748945266979242106/IMG_20200828_215650.jpg`,
    `https://cdn.discordapp.com/attachments/716917641209708647/748945228907413675/IMG_20200828_220208.jpg`
    ];
-   const embed = new Discord.MessageEmbed()
-   .setTitle("Here is your Nitro")
-   .setDescription(links[Math.floor(Math.random()*links.length)])
-   .setImage(images[Math.floor(Math.random()*images.length)])
-   .setColor("RANDOM");
-  message.channel.send(embed);
+  message.channel.send(`${links[Math.floor(Math.random()*links.length)]} \n${images[Math.floor(Math.random()*images.length)]}`);
 }
 }
