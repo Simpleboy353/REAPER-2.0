4fad64928e4529d950bf93cf3b6f9578b2e0f092
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-09-01 08:57:43 +0530

    Updated

--- Commands/nitro.js
+++ Commands/nitro.js
@@ -4,20 +4,20 @@ module.exports = {
    name: "nitro",
    description: "Fake a nitro gift",
    run: async(client, message, args) => {
-   function makeid(length) {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
+   var links = [
+   `https://discord.gg/Hejs82hejdi9`,
+   `https://discord.gg/ejf88rjcUw8i`,
+   `https://discord.gg/aujtjc68Wisa`,
+   `https://discord.gg/aueuhdjx8eo9`,
+   `https://discord.gg/aytjx1juy8Wf`
+    ];
    var images = [
    `https://cdn.discordapp.com/attachments/716917641209708647/748945266979242106/IMG_20200828_215650.jpg`,
    `https://cdn.discordapp.com/attachments/716917641209708647/748945228907413675/IMG_20200828_220208.jpg`
    ];
    const embed = new Discord.MessageEmbed()
    .setTitle("Here is your Nitro")
-   .setDescription(`https://discord.gg/${makeid(10)}`)
+   .setDescription(links[Math.floor(Math.random()*links.length)])
    .setImage(images[Math.floor(Math.random()*images.length)])
    .setColor("RANDOM");
   message.channel.send(embed);
