eb9d5935c996f72b199dd082d9762ecb02625133
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-09-06 18:07:01 +0530

    Updated

--- Commands/serverinfo2.js
+++ Commands/serverinfo2.js
@@ -1,5 +1,6 @@
 const Discord = module.require("discord.js");
-
+const moment = require("moment");
+require("moment-duration-format");
 module.exports = {
     name: "serverinfo2",
     category: "utility",
@@ -7,6 +8,8 @@ module.exports = {
     usage: "[command]",
     run: async (client, message, args) => {
         //command
+        const join = moment.duration(message.guild.joinedAt).format(" D [days], H [hrs], m [mins], s [secs]");
+        const create = moment.duration(message.guild.createdAt).format(" D [days], H [hrs], m [mins], s [secs]");
         let afk = message.guild.afkChannel;
         let achannel = "";
         if (afk) {
@@ -23,30 +26,30 @@ module.exports = {
             "VERY_HIGH": "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻ (Very High)"
         };
         let region = {
-            "brazil": "🇧🇷 Brazil",
-            "eu-central": "🇪🇺 Central Europe",
-            "singapore": "🇸🇬 Singapore",
-            "us-central": "🇺🇸 U.S. Central",
-            "sydney": "🇦🇺 Sydney",
-            "us-east": "🇺🇸 U.S. East",
-            "us-south": "🇺🇸 U.S. South",
-            "us-west": "🇺🇸 U.S. West",
-            "eu-west": "🇪🇺 Western Europe",
-            "vip-us-east": "🇺🇸 VIP U.S. East",
-            "london": "🇬🇧 London",
-            "amsterdam": "🇳🇱 Amsterdam",
-            "hongkong": "🇭🇰 Hong Kong",
-            "russia": "🇷🇺 Russia",
-            "southafrica": "🇿🇦  South Africa",
-            "india": "🇮🇳 India"
+            "brazil": "Brazil",
+            "eu-central": "Central Europe",
+            "singapore": "Singapore",
+            "us-central": "U.S. Central",
+            "sydney": "Sydney",
+            "us-east": "U.S. East",
+            "us-south": "U.S. South",
+            "us-west": "U.S. West",
+            "eu-west": "Western Europe",
+            "vip-us-east": "VIP U.S. East",
+            "london": "London",
+            "amsterdam": "Amsterdam",
+            "hongkong": "Hong Kong",
+            "russia": "Russia",
+            "southafrica": "South Africa",
+            "india": "India"
         };
     const serverembed = new Discord.MessageEmbed()
     .setTitle(`${message.guild.name}`, `${message.guild.iconURL()}`)
     .setThumbnail(servericon)
-    .addField(`General Info`, `Owner: ${message.guild.owner} \nRegion: **${region[message.guild.region]}** \nVerification Lvl: **${verifLevels[message.guild.verificationLevel]}**`)
-    .addField(`Overview`, `Total Channels: **${message.guild.channels.cache.size}** \nAFK Channel: ${message.guild.afkChannel} \nAFK Timeout: **${message.guild.afkTimeout}** \nTotal Roles: **${message.guild.roles.cache.size}**`)
-    .addField(`Member Info`, `Total Members: **${message.guild.memberCount}** \n Humans: **${message.guild.members.cache.filter(member => !member.user.bot).size}** \nBots: ${message.guild.members.cache.filter(member => member.user.bot).size}**`)
-    .addField(`Misc. Info`, `You Joined on: ${message.member.joinedAt} \nCreated On: ${message.guild.createdAt}`)
+    .addField(`General Info`, `Owner: ${message.guild.owner} \nRegion: \`${region[message.guild.region]}\` \nVerification Lvl: \`${verifLevels[message.guild.verificationLevel]}\`**`)
+    .addField(`Overview`, `Total Channels: \`${message.guild.channels.cache.size}\` \nAFK Channel: ${message.guild.afkChannel} \nAFK Timeout: \`${message.guild.afkTimeout}\` \nTotal Roles: \`${message.guild.roles.cache.size}\``)
+    .addField(`Member Info`, `Total Members: \`${message.guild.memberCount}\` \n Humans: \`${message.guild.members.cache.filter(member => !member.user.bot).size}\` \nBots: \`${message.guild.members.cache.filter(member => member.user.bot).size}\``)
+    .addField(`Misc. Info`, `You Joined on: \`${join}\` \nCreated On: \`${create}\``)
     .setThumbnail(message.guild.iconURL())
     .setFooter(`ID: ${message.guild.id}`, message.guild.iconURL())
     .setTimestamp();
