e574dc943ff174ba6bc6b064d8724fe61c23610f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-09-06 17:13:16 +0530

    Updated

--- Commands/wideavatar.js
+++ Commands/wideavatar.js
@@ -7,6 +7,6 @@ module.exports = {
     const mention = message.mentions.members.first() || message.member;
     const avatar = mention.user.displayAvatarURL({dynamic: true, size: 2048, format: "png"});
 
-        message.channel.send({ files: [{ attachment: `https://[https://vacefron.nl/api/wide?image=${avatar}`, name: "wideavatar.png"}]});
+        message.channel.send({ files: [{ attachment: `https://vacefron.nl/api/wide?image=${avatar}`, name: "wideavatar.png"}]});
     }
 }
