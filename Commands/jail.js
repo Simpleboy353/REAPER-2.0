bf49f31b823ef5f31861c7b649f54afdd23bd771
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-09-13 10:40:45 +0530

    Updated

--- Commands/jail.js
+++ Commands/jail.js
@@ -1,16 +1,19 @@
 const Discord = module.require("discord.js");
 const bot = new Discord.Client();
 const snek = require('snekfetch');
-const fs = require('fs');
+const fsn = require('fs-nextra');
 module.exports = {
     name: "jail",
-    description: "Another Image Manipulation Command",
+    description: "Image Manipulation Command",
     run: async (client, message, args) => {
     if (!message.guild.member(client.user).hasPermission('ATTACH_FILES')) return message.reply('Sorry, i dont have the perms to do this cmd i need ATTACH_FILES. :x:')
     const { Canvas } = require('canvas-constructor');
     if (message.mentions.users.size < 1) return message.channel.send("You didn't mention a user to put them behind bars");
     const getSlapped = async (person) => {
-        const plate = fs.readFile('./assets/plate_jail.png')
+        var plate = "/assets/plate_jail.png";
+        fsn.readFile(plate, function (err, obj) {
+            console.dir(obj)
+        })
         const png = person.replace('.gif', '.png');
         const { body } = await snek.get(png);
         return new Canvas(250, 250)
