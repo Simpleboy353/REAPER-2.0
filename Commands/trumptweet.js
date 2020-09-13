1abbc8d0f43fa432eb59061c810a46339e65cb86
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-09-13 19:15:59 +0530

    Updated

--- Commands/trumptweet.js
+++ Commands/trumptweet.js
@@ -7,7 +7,7 @@ module.exports = {
             usage: 'trumptweet <message>',
             description: 'Display\'s a custom tweet from Donald Trump with the message provided.',
             run: async(message, args) => {
-        const tweet = args.join(" ");
+        const tweet = args.slice(0).join(" ");
         if (!tweet) {
             return message.channel.send("Mr. President Says: \`What to tweet ?\`")
         }
