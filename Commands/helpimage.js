e9257de3480342ef9e8f2c68729ac7235f33983d
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-09-13 19:36:26 +0530

    Updated

--- Commands/helpimage.js
+++ Commands/helpimage.js
@@ -7,7 +7,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
     .setColor("RANDOM")
     .setTitle("Image Commands")
-    .setDescription("`achievement`, `amazeme`, `amiajoke`, `bad`, `catsay`, `challenge`, `changemymind`, `didyoumean`, `drake`, `facts`, `illegal`, `jokeoverhead`, `meme`, `pornhub`, `rip`, `scroll`, `tableflip`, `textimage`, `trash`, `wideavatar`");
+    .setDescription("`achievement`, `amazeme`, `amiajoke`, `bad`, `catsay`, `challenge`, `changemymind`, `didyoumean`, `drake`, `facts`, `illegal`, `jokeoverhead`, `meme`, `pornhub`, `rip`, `scroll`, `tableflip`, `textimage`, `trash`, `trumptweet`, `wideavatar`");
     message.channel.send(embed);
    }
 }
