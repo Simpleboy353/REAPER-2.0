48eae975ad1f7ff56478491f9d544e98be5a06ac
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-09-18 18:03:03 +0530

    Updated

--- Commands/creatememe.js
+++ Commands/creatememe.js
@@ -8,15 +8,15 @@ module.exports = {
         if (!memetemplate) {
             return message.channel.send("To see the available meme templates, type `=memetemplates`");
         }
-        const memetext1 = args.slice(1).join(" ");
+        const memetext1 = args[1];
         if (!memetext1) {
             return message.channel.send("Enter the text to be placed at the top!");
         }
-        const memetext2 = args.slice(2).join(" ");
+        const memetext2 = args[2];
         if (!memetext2) {
             return message.channel.send("Enter the text to be placed at the bottom!");
         }
-        message.channel.send({ files: [{ attachments: `https://api.memegen.link/images/${memetemplate}/${memetext1}/${memetext2}`, name: "custommeme.png"}]});
+        message.channel.send({ files: [{ attachment: `https://api.memegen.link/images/${memetemplate}/${memetext1}/${memetext2}`, name: "custommeme.png"}]})
     }, catch (error) {
         const errorlogs = client.channels.cache.get("747750993583669258")
         message.channel.send("Seems like an error has occured!. Please try again in a few hours!")
