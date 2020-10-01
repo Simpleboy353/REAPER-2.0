11e3b45d9bd7f01da159e5dd5478cb64eaae6172
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-10-01 08:13:36 +0530

    Updated

--- Commands/partnerships.js
+++ Commands/partnerships.js
@@ -8,7 +8,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
         .setThumbnail(avatar)
         .setTitle("Infinity Partners")
-        .addField(`**__Partner Servers__**`, `**__[/r/Champions](https://discord.gg/UnEU6Pc)__**\n**__[Infinity's Infinite Loop](https://discord.gg/X9PAszD)__**`)
+        .addField(`**__Partner Servers__**`, `**__[/r/Champions](https://discord.gg/UnEU6Pc)__**\n**__[Younger Development](https://discord.gg/mqWprFc)__**`)
         .addField(`**__Partner Bots__**`, `**__[Beats](https://discord.com/oauth2/authorize?client_id=730683152720199700&permissions=37223745&scope=bot)__**`)
         .setFooter(`Contact ๖ۣۜℜⱥjͥƤuͣtͫ#5915 to become a partner`)
         .setThumbnail(client.user.avatarURL())
