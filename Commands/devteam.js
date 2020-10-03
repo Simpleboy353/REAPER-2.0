5cfddf99a58fcfa5d04397d715919e82212828a5
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-03 18:02:08 +0530

    Updated

--- Commands/devteam.js
+++ Commands/devteam.js
@@ -6,7 +6,7 @@ module.exports = {
     run: async(client, message, args) => {
         const embed = new Discord.MessageEmbed()
         .setTitle("My Dev Team")
-        .setDescription("__**MAIN DEV AND OWNER**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_#0001\n\n__**BASE CODE EDITOR**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_#0001\n\n__**MUSIC CODE EDITOR**__\n<@549930183126286359> | SYNDROME\n\n__**DB HELPER**__\n<@728211572446461973> | GHOST RAZOR")
+        .setDescription("__**MAIN DEV AND OWNER**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**BASE CODE EDITOR**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**MUSIC CODE EDITOR**__\n<@549930183126286359> | SYNDROME\n\n__**DB HELPER**__\n<@728211572446461973> | GHOST RAZOR")
         .setColor("RANDOM");
         message.channel.send(embed);
     }
