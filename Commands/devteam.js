a094d9d5d84d5593a153094eb776f168ce00e7f0
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-14 15:06:33 +0530

    Updated

--- Commands/devteam.js
+++ Commands/devteam.js
@@ -6,7 +6,7 @@ module.exports = {
     run: async(client, message, args) => {
         const embed = new Discord.MessageEmbed()
         .setTitle("Infinity's Developers")
-        .setDescription("**MY DEV TEAM**\n__**MAIN DEV AND OWNER**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**BASE CODE EDITOR**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**MUSIC CODE EDITOR**__\n<@549930183126286359> | SYNDROME\n\n__**DB EDITOR**__\n<@728211572446461973> | GHOST RAZOR")
+            .setDescription("**MY DEV TEAM**\n__**MAIN DEV AND OWNER**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**BASE CODE EDITOR**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**MUSIC CODE EDITOR**__\n<@549930183126286359> | SYNDROME\n\n__**MANAGER**__\n<@587828386466889730> | FRAWZTY\n\n**DB EDITOR**__\n<@728211572446461973> | GHOST RAZOR")
         .setColor("RANDOM");
         message.channel.send(embed);
     }
