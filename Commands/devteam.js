ab36c55777a0fe147985ff49d22cded21c5e9313
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-14 15:11:15 +0530

    Updated

--- Commands/devteam.js
+++ Commands/devteam.js
@@ -4,9 +4,12 @@ module.exports = {
     name: "devteam",
     description: "Get the list for my dev team",
     run: async(client, message, args) => {
+        const avatar = client.user.avatarURL();
         const embed = new Discord.MessageEmbed()
-        .setTitle("Infinity's Developers")
-            .setDescription("**MY DEV TEAM**\n__**MAIN DEV AND OWNER**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**BASE CODE EDITOR**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**MUSIC CODE EDITOR**__\n<@549930183126286359> | SYNDROME\n\n__**MANAGER**__\n<@587828386466889730> | FRAWZTY\n\n**DB EDITOR**__\n<@728211572446461973> | GHOST RAZOR")
+        .setAuthor("Infinity's Developers", client.user.avatarURL())
+        .setThumbnail(avatar)
+        .setDescription("**MY DEV TEAM**\n__**MAIN DEV AND OWNER**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**BASE CODE EDITOR**__\n<@661501985517862972> | _๖ۣۜℜⱥjͥƤuͣtͫ_\n\n__**MUSIC CODE EDITOR**__\n<@549930183126286359> | SYNDROME\n\n__**MANAGER**__\n<@587828386466889730> | FRAWZTY\n\n**DB EDITOR**__\n<@728211572446461973> | GHOST RAZOR")
+        .setThumbnail(client.user.avatarURL)
         .setColor("RANDOM");
         message.channel.send(embed);
     }
