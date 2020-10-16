61ee7642f058f9f54a9dbee82b9bc284420af12a
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-16 18:28:37 +0530

    Updated

--- Commands/voicemute.js
+++ Commands/voicemute.js
@@ -1,22 +1,30 @@
-const Discord = module.require("discord.js");
+const discord = require("discord.js");
 
 module.exports = {
     name: "voicemute",
-    description: "mute a user from a voice channel",
+    description: "Mute Users from Voice Channels!",
     run: async (client, message, args) => {
-        if (!message.guild.me.hasPermission(["ADMINISTRATOR"])) {
-            return message.channel.send("I Don't Have Proper Permissions To Use This Command!");
-        }
-        if (!message.mentions.members.first()) {
-            return message.channel.send(`Please Mention User That You Want To mute From Voice Channel!`);
-        }
-        let { channel } = message.mentions.members.first().voice;
+    var a = message.id;
+    if (!message.member.hasPermission('MUTE_MEMBERS')) return message.reply("You do not have the permission to do that!");
+    if (!message.member.voice.channel) return message.reply("You are not in a voice channel!");
+    var user = message.mentions.members.first();
 
-        if (!channel) {
-            return message.channel.send(`User Is Not In Any Voice Channel!`);
+    if (!user) return message.reply("Whom do you wanna mute?");
+    let channel = message.member.voice.channel;
+    var found = 0;
+    for (let memberi of channel.members) {
+        if (memberi[1] == user) {
+            found++;
         }
-        message.mentions.members.first().voice.setMute();
-
-        message.channel.send(`User Has Been Mutes From Voice Channel!`)
     }
+    if (found == 1) {
+        await user.voice.setMute(true);
+        message.channel.send(`${user} muted by ${message.author}`);
+        message.channel.messages.fetch(a).then(msg => msg.delete({ timeout: 1000 }));
+    }
+    else {
+        message.channel.send("You are not in the same channel!!");
+        message.channel.messages.fetch(a).then(msg => msg.delete({ timeout: 1000 }));
+    }
+}
 };
