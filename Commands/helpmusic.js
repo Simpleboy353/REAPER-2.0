8e68c075a585cae20f2debf30842455da3530a3b
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-10-16 08:45:16 +0530

    Updated

--- Commands/helpmusic.js
+++ Commands/helpmusic.js
@@ -6,7 +6,7 @@ module.exports = {
     run: async(client, message, args) => {
         const embed = new Discord.MessageEmbed()
         .setTitle("Music Commands")
-        .setDescription("`play [p]`, `pause`, `resume [r]`, `skip [s]`, `shuffle`, `skipto [st]`, `search`, `queue [q]`, `np`, `playlist [pl]`, `volume`, `pruning`, `remove`, `lyrics [ly]`, `loop [l]`")
+        .setDescription("`play [p]`, `pause`, `resume [r]`, `skip [s]`, `shuffle`, `skipto [st]`, `search`, `stop`, `queue [q]`, `np`, `playlist [pl]`, `volume`, `pruning`, `remove`, `lyrics [ly]`, `loop [l]`")
         .setColor("RANDOM")
         message.channel.send(embed);
     }
