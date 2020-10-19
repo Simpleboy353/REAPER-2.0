fbd61e14feab01da5d1f9fd28f93f30ac177e7ff
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-19 19:52:41 +0530

    Updated

--- index.js
+++ index.js
@@ -17,6 +17,24 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`)
 })
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({ status: 'online' });
+  const activities_list = [
+    { msg: "Infinity Rocks", type: "STREAMING" },
+    { msg: "music for your server!", type: "PLAYING" },
+    { msg: "=help", type: "LISTENING" },
+    { msg: "Helping You make your Server Better", type: "PLAYING" },
+    { msg: `with ${client.users.cache.size} users in ${client.guilds.cache.size} servers!`, type: "PLAYING" },
+  ];// creates an arraylist containing phrases you want your bot to switch through.
+  setInterval(() => {
+    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 
+      client.user.setActivity(activities_list[index].msg, {
+        type: activities_list[index].type,
+      }); // sets bot's activities to one of the phrases in the arraylist.
+  }, 10000); // Runs this every 10 seconds.});
+});
+
 client.on("message", async message => {
   client.prefix = prefix;
   if (message.author.bot) return; // This line makes sure that the bot does not respond to other bots
