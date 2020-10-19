fada16256c2c81c0cadac167ee8a443f255834ae
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-10-19 22:02:34 +0530

    Updated

---
+++ models/prefix.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const PrefixSchema = new mongoose.Schema({
+    Prefix: {
+        type: String
+    },
+    GuildID: String
+});
+
+const MessageModel = module.exports = mongoose.model('prefixes', PrefixSchema);
