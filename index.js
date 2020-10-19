0a22432dc1fde6c5787e338cc3ef6d203a8032ca
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-19 19:20:02 +0530

    Updated

--- index.js
+++ index.js
@@ -28,23 +28,24 @@ client.on("message", async message => {
   const cmd = args.shift().toLowerCase();
   if (cmd.length === 0) return;
 
+  const embed = new MessageEmbed()
+    .setTitle('Commands List')
+    .setDescription("Available Categories: ")
+    .addField(`<a:ColorDino:726964382009131099> __**Fun**__: `, "`=helpfun`\nUse these commands to have some fun in your Server")
+    .addField(`<:cam:748544442478100511> __**Image**__: `, "`=helpimage`\nManipulate Images with these commands")
+    .addField(`<:inf:748544269798866964> __**Info**__: `, "`=helpinfo`\nHave some info relating the Server, Users or Our Bot")
+    .addField(`<:mod:748544387499294841> __**Moderation**__: `, "`=helpmod`\nIssues relating some users? Use these Commands")
+    .addField(`<:music:761893108442071060> __**Music**__: `, "`=helpmusic`\nRelax and listen to some music!")
+    .addField(`<:utility:748177830134808597> __**Utility**__: `, "`=helputility`\nSome Simple Utility Commands")
+    .setColor("RANDOM");
+  if (message.content === "=help"){
+    message.channel.send(embed)
+  }
+
   let command = client.commands.get(cmd);
   if (!command) command = client.commands.get(client.aliases.get(cmd));
   if (command)
     command.run(client, message, args);
 })
-const embed = new MessageEmbed()
-  .setTitle('Commands List')
-  .setDescription("Available Categories: ")
-  .addField(`<a:ColorDino:726964382009131099> __**Fun**__: `, "`=helpfun`\nUse these commands to have some fun in your Server")
-  .addField(`<:cam:748544442478100511> __**Image**__: `, "`=helpimage`\nManipulate Images with these commands")
-  .addField(`<:inf:748544269798866964> __**Info**__: `, "`=helpinfo`\nHave some info relating the Server, Users or Our Bot")
-  .addField(`<:mod:748544387499294841> __**Moderation**__: `, "`=helpmod`\nIssues relating some users? Use these Commands")
-  .addField(`<:music:761893108442071060> __**Music**__: `, "`=helpmusic`\nRelax and listen to some music!")
-  .addField(`<:utility:748177830134808597> __**Utility**__: `, "`=helputility`\nSome Simple Utility Commands")
-  .setColor("RANDOM");
-if (message.content === `${prefix}help`) {
-  message.channel.send(embed);
-}
 
 client.login(process.env.token)//Enter your bot token here
