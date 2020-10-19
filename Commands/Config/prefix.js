6a8e36451dec5eeedb7a07a5fca0f0424271e8e8
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-10-19 21:46:08 +0530

    Updated

---
+++ Commands/Config/prefix.js
@@ -0,0 +1,37 @@
+const prefixModel = require("../models/data")
+
+module.exports = {
+    name: "prefix",
+    description: "Set Custom Prefix",
+    run: async (client, message, args) => {
+    const data = await prefixModel.findOne({
+        GuildID: message.guild.id
+    });
+
+    if (!args[0]) return message.channel.send('You must provide a **new prefix**!');
+
+    if (args[0].length > 5) return message.channel.send('Your new prefix must be under \`5\` characters!')
+
+    if (data) {
+        await prefixModel.findOneAndRemove({
+            GuildID: message.guild.id
+        })
+        
+        message.channel.send(`The new prefix is now **\`${args[0]}\`**`);
+
+        let newData = new prefixModel({
+            Prefix: args[0],
+            GuildID: message.guild.id
+        })
+        newData.save();
+    } else if (!data) {
+        message.channel.send(`The new prefix is now **\`${args[0]}\`**`);
+
+        let newData = new prefixModel({
+            Prefix: args[0],
+            GuildID: message.guild.id
+        })
+        newData.save();
+    }
+}
+}
