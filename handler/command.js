8dad1a851c58dd6206b092dc49fb27dd5b198ed1
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-19 18:49:54 +0530

    Updated

--- handler/command.js
+++ handler/command.js
@@ -5,10 +5,10 @@ const asciiTable = require("ascii-table");
 const table = new ascii().setHeading('Commands', 'Load Status');
 
 module.exports = (client) => {
-    readdirSync('./commands/').forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(f => f.endsWith('.js'));
+    readdirSync('./Commands/').forEach(dir => {
+        const commands = readdirSync(`./Commands/${dir}/`).filter(f => f.endsWith('.js'));
         for (let file of commands) {
-            let pull = require(`../commands/${dir}/${file}`);
+            let pull = require(`../Commands/${dir}/${file}`);
             if (pull.name) {
                 client.commands.set(pull.name, pull);
                 table.addRow(file, '✔️')
