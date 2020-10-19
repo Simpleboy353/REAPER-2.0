c0ef487d621c38a5852b0a93c2103d3b9c0b4f53
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-19 19:10:46 +0530

    Updated

--- index.js
+++ index.js
@@ -33,5 +33,18 @@ client.on("message", async message => {
   if (command)
     command.run(client, message, args);
 })
+const embed = new Discord.MessageEmbed()
+  .setTitle('Commands List')
+  .setDescription("Available Categories: ")
+  .addField(`<a:ColorDino:726964382009131099> __**Fun**__: `, "`=helpfun`\nUse these commands to have some fun in your Server")
+  .addField(`<:cam:748544442478100511> __**Image**__: `, "`=helpimage`\nManipulate Images with these commands")
+  .addField(`<:inf:748544269798866964> __**Info**__: `, "`=helpinfo`\nHave some info relating the Server, Users or Our Bot")
+  .addField(`<:mod:748544387499294841> __**Moderation**__: `, "`=helpmod`\nIssues relating some users? Use these Commands")
+  .addField(`<:music:761893108442071060> __**Music**__: `, "`=helpmusic`\nRelax and listen to some music!")
+  .addField(`<:utility:748177830134808597> __**Utility**__: `, "`=helputility`\nSome Simple Utility Commands")
+  .setColor("RANDOM");
+if (message.content === `${prefix}help`) {
+  message.channel.send(embed);
+}
 
 client.login(process.env.token)//Enter your bot token here
