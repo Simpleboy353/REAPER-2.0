c0e487f43b39d70672010ab1919539a8289c085a
AgentX | SYNDROME <66668281+AgentXtreme@users.noreply.github.com>
2020-10-20 11:54:00 +0530

    Update prefix.js

--- Commands/Config/prefix.js
+++ Commands/Config/prefix.js
@@ -1,40 +1,42 @@
-const Discord = module.require("discord.js")
-const prefixModel = require("../Owner/models/prefix");
+const mongoose = require("mongoose")
+const config = require("../../config.json")
+const {MessageEmbed} = require("discord.js")
+const ms = require('parse-ms')
+
+mongoose.connect(config.mongoPass,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+const Data = require('../../models/serverdata.js')
+
 
 module.exports = {
-  name: "prefix",
-  description: "Change the prefix per server!",
-  run: async(client, message, args) => {
-  const data = await prefixModel.findOne({
-    GuildID: message.guild.id
-  });
-
-  if (!args[0])
-    return message.channel.send('You must provide a **new prefix**!');
-
-  if (args[0].length > 5)
-    return message.channel.send('Your new prefix must be under \`5\` characters!');
-
-  if (data) {
-    await prefixModel.findOneAndRemove({
-      GuildID: message.guild.id
-    });
-
-    message.channel.send(`The new prefix is now **\`${args[0]}\`**`);
-
-    let newData = new prefixModel({
-      Prefix: args[0],
-      GuildID: message.guild.id
-    });
-    newData.save();
-  } else if (!data) {
-    message.channel.send(`The new prefix is now **\`${args[0]}\`**`);
-
-    let newData = new prefixModel({
-      Prefix: args[0],
-      GuildID: message.guild.id
-    });
-    newData.save();
-  }
-}
+    name: "prefix",
+    description: "Change the bot prefix",
+    run: async (client, message, args) => {
+        Data.findOne({
+            gid:message.guild.id
+        },(err,data)=>{
+            if(err) console.log(err);
+            if(!data){
+const newD = new Data({
+              gid:message.guild.id,
+          gname:message.guild.name,
+              gprefix:"&"
+           })
+newD.save().catch(err => console.log(err));
+}else{
+     if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("You don't have permissions to set prefix!"); //If The Author Doesnt Have Manage guild permission return a message
+      if(!args[0]) return message.channel.send("Please specify a prefix!"); //If there isn't a prefix then return a message
+      
+    let oldp = data.gprefix;
+    
+       data.gprefix = data.gprefix.replace(oldp, args[0]);;
+    data.save().catch(err => console.log(err));
+      message.channel.send(`Prefix has been set to **${args[0]}**!`); //send message to that channel
+      return; //return
+    }
+    }
+                     )
+    }
 }
