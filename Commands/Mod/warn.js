ce5a2d14cd63fb1d94155d73a1794365f755297f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-20 17:31:19 +0530

    Updated

---
+++ Commands/Mod/warn.js
@@ -0,0 +1,33 @@
+const Discord = module.require("discord.js")
+const prefixModel = require("../Owner/models/warns");
+
+module.exports = {
+  name: "warn",
+  description: "Change the prefix per server!",
+  run: async (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_SERVER")) {
+      return message.channel.send("You don't have enough Permissions!")
+    }
+    const target = message.mentions.members.first();
+    if (!target) {
+      return message.channel.send("Mention a user!")
+    }
+    const data = await prefixModel.findOne({
+      UserID: target.id,
+      GuildID: message.guild.id
+    });
+
+    if (data) {
+      prefixModel.Warns += 1
+      message.channel.send(`**${target.user.username}** has been warned! Total warnings: ${data.Warns}`);
+    } else if (!data) {
+      let newData = new prefixModel({
+        Warns: 1,
+        UserID: target.id,
+        GuildID: message.guild.id
+      });
+      newData.save();
+      message.channel.send(`**${target.user.username}** has been warned! Total warnings: ${data.Warns}`)
+    }
+  }
+}
