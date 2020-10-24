18170773343689d7424c469b69803a067126bffa
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-10-24 10:20:50 +0530

    Updated

--- Commands/Image/phb.js
+++ Commands/Image/phb.js
@@ -3,14 +3,17 @@ const Jimp = require("jimp");
 const cooldown = new Set();
 
 module.exports = {
-    name: "pornhub",
+    name: "phb",
     description: "Another Fun Command",
     run: async(client, message, args) => {
             if (message.channel.type === "dm") return;
             let text = args[0];
             let text2 = args[1];
             if (!text) {
-                return message.reply('You need to provide some text!');
+                return message.reply('You need to provide the first argument!');
+            }
+            if (!text2) {
+                return message.channel.send('You need to provide the second argument!')
             }
             message.channel.send({files : [{attachment: `https://api.alexflipnote.dev/pornhub?text=${text}&text2=${text2}`, name: 'file.jpg'}]})
     message.delete();
