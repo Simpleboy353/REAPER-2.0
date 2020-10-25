e749096584413f2912006bcbd823bf8c572395f4
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-10-25 10:13:05 +0530

    Updated

--- Commands/Utility/addemoji.js
+++ Commands/Utility/addemoji.js
@@ -4,36 +4,40 @@ const { MessageEmbed } = require("discord.js");
 const Color = `RANDOM`;
 
 module.exports = {
-    name: "addemoji",
-    category: "Administrators",
-    run: async (client, message, args) => {
-
-        if (!message.member.hasPermission(`MANAGE_EMOJIS`)) {
-            return message.channel.send(`You Don't Have Permission To Use This Command! Manage Emojis`)
-        }
-
-        const emoji = args[0];
-        if (!emoji) return message.channel.send(`Please Give Me A Emoji!`);
-         
-        let customemoji = Discord.Util.parseEmoji(emoji);
-
-       
-        if (customemoji.id) {
-            const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${customemoji.animated ? "gif" : "png"
-                }`;
-            const name = args.slice(0).join(" ");
-            message.guild.emojis.create(
-                `${Link}`,
-                `${name || `${customemoji.name}`}`
-            );
-            return message.channel.send(`Emoji added with the name \`${customemoji.name}\`!`);
-        } else {
-            let CheckEmoji = parse(emoji, { assetType: "png" });
-            if (!CheckEmoji[0])
-                return message.channel.send(`Please Give Me A Valid Emoji!`);
-            message.channel.send(
-                `You Can Use Normal Emoji Without Adding In Server!`
-            );
-        }
+  name: "stealemoji",
+  category: "Administrators",
+  run: async (client, message, args) => {
+    
+    if (!message.member.hasPermission(`MANAGE_EMOJIS`)) {
+      return message.channel.send(`You Don't Have Permission To Use This Command! Manage Emojis`)
     }
+    
+    const emoji = args[0];
+    if (!emoji) return message.channel.send(`Please Give Me A Emoji!`);
+
+    let customemoji = Discord.Util.parseEmoji(emoji);
+
+    if (customemoji.id) {
+      const Link = `https://cdn.discordapp.com/emojis/${customemoji.id}.${
+        customemoji.animated ? "gif" : "png"
+      }`;
+      const name = args.slice(1).join(" ");
+      message.guild.emojis.create(
+        `${Link}`,
+        `${name || `${customemoji.name}`}`
+      );
+      const Added = new MessageEmbed()
+        .setTitle(`Emoji Added`)
+        .setColor(`${Color}`)
+        .setDescription(`Emoji added with the name \`${name || customemoji.name}\``);
+      return message.channel.send(Added);
+    } else {
+      let CheckEmoji = parse(emoji, { assetType: "png" });
+      if (!CheckEmoji[0])
+        return message.channel.send(`Please Give Me A Valid Emoji!`);
+      message.channel.send(
+        `You Can Use Normal Emoji Without Adding In Server!`
+      );
+    }
+  }
 };
