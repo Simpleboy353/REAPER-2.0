ea88f538a1fb1cf203b9bec0dd2747c3b651b79a
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-25 14:00:32 +0530

    Updated

--- Commands/Info/help.js
+++ Commands/Info/help.js
@@ -28,14 +28,15 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
       .setThumbnail(avatar)
       .setTitle("Available Command Categories")
-      .addField(`<:conf:748544324978999448> __**CONFIGURATION**__: `, `\`${prefix}help config\`\nConfigure the bot as per your server!`)
-      .addField(`<a:ColorDino:726964382009131099> __**FUN**__: `, `\`${prefix}help fun\`\nHave some fun in your Server!`)
-      .addField(`<:cam:748544442478100511> __**IMAGE**__: `, `\`${prefix}help image\`\nManipulate Images with these commands`)
-      .addField(`<:inf:748544269798866964> __**INFORMATION**__: `, `\`${prefix}help info\`\n Get some info relating the Server, users or our Bot`)
-      .addField(`<:mod:748544387499294841> __**MODERATION**__: `, `\`${prefix}help mod\`\nIssues relating some users? Use these Commands`)
-      .addField(`<:music:761893108442071060> __**MUSIC**__: `, `\`${prefix}help music\`\nRelax and listen to some music!\nNote: Music Commands work with the default prefix \`=\` only!`)
-      .addField(`<:nsfw:768346751576440852> __**NSFW**__: `, `\`${prefix}help nsfw\`\nSome NSFW Content for you!`)
-      .addField(`<:utility:748177830134808597> __**UTILITIES**__: `, `\`${prefix}help utility\`\nSome Simple Utility Commands`)
+      .setDescription(`
+<:conf:748544324978999448> __**CONFIGURATION**__: `, `\`${prefix}help config\`\nConfigure the bot as per your server!
+<a:ColorDino:726964382009131099> __**FUN**__: `, `\`${prefix}help fun\`\nHave some fun in your Server!
+<:cam:748544442478100511> __**IMAGE**__: `, `\`${prefix}help image\`\nManipulate Images with these commands
+<:inf:748544269798866964> __**INFORMATION**__: `, `\`${prefix}help info\`\nGet some info relating the Server, users or our Bot
+<:mod:748544387499294841> __**MODERATION**__: `, `\`${prefix}help mod\`\nIssues relating some users? Use these Commands
+<:music:761893108442071060> __**MUSIC**__: `, `\`${prefix}help music\`\nRelax and listen to some music!\nNote: Music Commands work with the default prefix \`=\` only!
+<:nsfw:768346751576440852> __**NSFW**__: `, `\`${prefix}help nsfw\`\nSome NSFW Content for you!
+<:utility:748177830134808597> __**UTILITIES**__: `, `\`${prefix}help utility\`\nSome Simple Utility Commands`)
       .setThumbnail(client.user.avatarURL())
       .setColor("GREEN");
 
