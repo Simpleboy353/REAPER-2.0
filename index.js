93301e9b791f2799d66ca53e413a6fe7a31f5383
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-10-31 09:08:11 +0530

    Updated

--- index.js
+++ index.js
@@ -249,19 +249,15 @@ client.on('guildMemberUpdate', async (oldMember, newMember)=>  {
    const  modlogs = data.Mod
   var Changes = {
     AddedRoles: 1,
-    RemovedRoles: 2,
-    username: 3,
-    nickname: 4,
-    avatar: 5
+    username: 2,
+    nickname: 3,
+    avatar: 4
   }
   var change = Changes.unknown
 
   // check if roles were removed
   if (newMember.roles.cache.size != oldMember.roles.cache.size) {
    change = Changes.AddedRoles
-  }
-  if (newMember.roles.cache.size != oldMember.roles.cache.size) {
-    change = Changes.RemovedRoles
   }
   // check if username changed
   if (newMember.user.username != oldMember.user.username) {
@@ -291,21 +287,6 @@ client.on('guildMemberUpdate', async (oldMember, newMember)=>  {
         }
       }
         newMember.guild.channels.cache.get(modlogs).send(embed)
-        break
-      case Changes.RemovedRoles:
-        let embed6 = new MessageEmbed()
-          .setTitle(newMember.user.tag)
-          .setThumbnail(avatar)
-          .setDescription(`**📝 User Roles Updated**`)
-          .setColor("RED")
-          .setThumbnail(newMember.user.avatarURL())
-          .setTimestamp();
-        for (const role of oldMember.roles.cache.map(x => x.id)) {
-          if (!newMember.roles.cache.has(role)) {
-            embed6.addField(`Role Removed`, `${newMember.roles.cache.get(role)}`, true)
-          }
-        }
-        newMember.guild.channels.cache.get(modlogs).send(embed6)
         break
       case Changes.username:
         let embed2 = new MessageEmbed()
