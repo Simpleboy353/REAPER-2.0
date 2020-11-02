0d701f0d9c79d854a7076ff340de878a0ae51991
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-11-02 13:48:49 +0530

    Updated

--- Commands/Owner/eval.js
+++ Commands/Owner/eval.js
@@ -5,9 +5,18 @@ module.exports = {
   description: "Evals JS a code",
   run: async(client, message, args)=>{
     if (message.author.id != "661501985517862972") {
+      var cmd = args.join(" ")
+      if (!cmd) {
+        return message.channel.send("What to Eval?");
+      } 
       return message.channel.send("This is an developer only command!")
     }
-    const result = eval(content.replace('=eval', ""))
-    message.chanel.send(result)
+    const result = eval(cmd)
+    let embed = new Discord.MessageEmbed()
+    .setTitle("Eval")
+    .addField(`Input`, cmd)
+    .addField(`Output`, result)
+    .setColor("GREEN")
+    message.chanel.send(embed)
   }
 }
