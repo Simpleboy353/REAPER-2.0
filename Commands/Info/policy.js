516c7c22de9bfa2eee7b940265201b9ad4f3c345
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-11-04 11:47:50 +0530

    Updated

--- Commands/Info/policy.js
+++ Commands/Info/policy.js
@@ -9,7 +9,7 @@ module.exports = {
     .setescription("By using the bot you agree to our privacy policy")
     .addField("What data do we store?", `
     \`We do not collect any personal information relating your account like Passwords or any other Credentials. The data we collect includes the Server IDs, User IDs, Channel IDs and Some Role IDs.The bot never requests for any personal data of the users and any act that breaks the Tos of Discord is discouraged by us!\``)
-    .addField("Why we need this data?" `
+    .addField("Why we need this data?", `
     \`The data is required for the proper fucntioning of the bot features like logging and autoroles. Without this data, our bot will not be able to perform the logging activities and thus making the features not accessable for users\``)
     .addField(`How long do we store your data?`, `
     \`The data is stored as long as the bot is your Server as soon as the bot is kicked or removed from the Server, the data is deleted and is no longer to accessable to anyone\``)
