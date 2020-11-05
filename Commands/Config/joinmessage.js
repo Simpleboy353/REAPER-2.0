a1d1c6d4f315d76743afefd3e610d2d5b16faa4d
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-11-05 19:30:32 +0530

    Updated

--- Commands/Config/joinmessage.js
+++ Commands/Config/joinmessage.js
@@ -12,7 +12,7 @@ module.exports = {
     if (!args[0]) {
       return message.channel.send(`\`Usage: =joinmessage <Text|off>\``)
     }
-    if (text) {
+    if (text !== "off") {
       const data = await prefixModel.findOne({
         GuildID: message.guild.id
       });
@@ -38,7 +38,7 @@ module.exports = {
         message.channel.send(`Join Message set to ${data.JoinMsg}`);
 
       }
-    } else if (args[0] === "off") {
+    } else if (text === "off") {
       const data2 = await prefixModel.findOne({
         GuildID: message.guild.id
       });
