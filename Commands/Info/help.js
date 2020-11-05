d6d2799fd095966953beeaf99b4c6736f0a11c33
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-11-05 20:34:13 +0530

    Updated

--- Commands/Info/help.js
+++ Commands/Info/help.js
@@ -86,7 +86,7 @@ module.exports = {
       const embed5 = new Discord.MessageEmbed()
         .setTitle("Config Commands")
         .setDescription(
-`To see current settings do \`${prefix}config stats\`\n **Autorole & Prefix**\n\`\`\`▪ autorole <role|off>\nprefix <NewPrefix>\`\`\`\n **Join & Leave**\n\`\`\`▪ joinchannel <#channel|off>\n▪ joinmessage <Text|off>\n    •••••••••••••••    \n▪ leavechannel <#channel|off>\n▪ leavemessage <Text|off>\`\`\`\n **Logs**\`\`\`▪ messagelogs <#channel|off>\n▪ modlogs <#channel|off>\`\`\`\n\`\`\`Note: Custom Join and Leave messages are in beta and doesn't include any variables yet! Therefore bot will mention users itself! And if you setup the join and leave channels without custom messages, the bot will send automated response when user joins or leaves!\`\`\``)
+`To see current settings do \`${prefix}config stats\`\n **Autorole & Prefix**\n\`\`\`▪ autorole <role|off>\n▪ prefix <NewPrefix>\`\`\`\n **Join & Leave**\`\`\`▪ joinchannel <#channel|off>\n▪ joinmessage <Text|off>\n▪ leavechannel <#channel|off>\n▪ leavemessage <Text|off>\`\`\`\n **Logging Activites**\`\`\`▪ messagelogs <#channel|off>\n▪ modlogs <#channel|off>\`\`\`\n\`\`\`Note: Custom Join and Leave messages are in beta and doesn't include any variables yet! Therefore bot will mention users itself! And if you setup the join and leave channels without custom messages, the bot will send automated response when user joins or leaves!\`\`\``)
         .setFooter("New Commands Coming Soon!")
         .setColor("RANDOM");
 
