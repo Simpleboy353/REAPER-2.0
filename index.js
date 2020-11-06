2258aa60b64d3a2df4653e944a55016f1a1af468
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-11-06 18:18:44 +0530

    Updated

--- index.js
+++ index.js
@@ -50,7 +50,16 @@ client.on('message', async (message) => {
       .setDescription("Hello Everyone, I am Infinity, A multi-purpose Discord Bot \n\nMy default Prefix is `=` but you can change it accordingly. To get my Commands List type `=help`\n\nIf you want to report any error, you can use the `=report` command and if you want to suggest features for me, you can use the `=suggest` command!")
       .addField("Some Useful Links", "Get my Invite Link [Here](https://discord.com/oauth2/authorize?client_id=733670294086221865&permissions=1584921983&scope=bot)\nNeed Assistance? Join my [Support Server](https://discord.gg/mqWprFc) Now!")
       .setColor("GREEN")
-    guild.systemChannel.send(embed100);
+    message.channel.send(embed100);
+  }
+
+  if (message.content.match(`<@!${client.user.id}>`)) {
+    let embed101 = new MessageEmbedEmbed()
+      .setTitle("Thanks for the Ping!")
+      .setDescription("Hello Everyone, I am Infinity, A multi-purpose Discord Bot \n\nMy default Prefix is `=` but you can change it accordingly. To get my Commands List type `=help`\n\nIf you want to report any error, you can use the `=report` command and if you want to suggest features for me, you can use the `=suggest` command!")
+      .addField("Some Useful Links", "Get my Invite Link [Here](https://discord.com/oauth2/authorize?client_id=733670294086221865&permissions=1584921983&scope=bot)\nNeed Assistance? Join my [Support Server](https://discord.gg/mqWprFc) Now!")
+      .setColor("GREEN")
+    message.channel.send(embed101);
   }
   //Getting the data from the model
   const data = await prefix.findOne({
