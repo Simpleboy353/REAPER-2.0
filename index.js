fbdbf21f3ceb567ba9a8e5bf275b6a382b400899
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-11-07 20:10:05 +0530

    Updated

--- index.js
+++ index.js
@@ -169,9 +169,7 @@ client.on(`guildMemberAdd`, async (member) => {
      ctx.fillText(member.guild.name, 408, 358)
      let channel = data.Welcome
      member.guild.channels.cache.get(channel).send(`${member}, Welcome to **${member.guild.name}**! We hope you like our Server! Enjoy Your Stay here!`)
-     let embed10 = new MessageEmbed()
-      .setImage(canvas.toBuffer())
-     member.guild.channels.cache.get(channel).send(embed10);
+     member.guild.channels.cache.get(channel).send({ files: [{ attachment: canvas.toBuffer(), name: 'welcome.png' }] });
 
      var errorlogs = client.channels.cache.get("747750993583669258")
    } catch (err) {
