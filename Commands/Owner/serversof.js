09cc344d0f74639d1b374043ad1ae384311047be
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-11-09 14:59:22 +0530

    Updated

---
+++ Commands/Owner/serversof.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+
+require('dotenv');
+
+module.exports = {
+  name: "serversof",
+  description: "Check what Servers the bot is in!",
+  run: async (client, message, args) => {
+    let id = args[0];
+    if (!id) return message.channel.send("Enter an ID")
+    try {
+      if (message.author.id != "661501985517862972") return message.channel.send(`<a:_cross:725303285015117844> Developer Only <a:_cross:725303285015117844>`);
+      let data = [];
+      client.guilds.cache.forEach(x => {
+        if (x.owner.id == id) {
+        message.channel.send(`🔹**${x.name}** | \`${x.memberCount}\` members (ID: ${x.id})\n............................`);
+        }
+      });
+
+      if (data.length > 0) {
+        data.sort();
+        data = `🔹 ` + data.join('\n🔹');
+      }
+      else {
+        data = "[No server found]";
+      }
+    } catch (err) {
+
+      const errorlogs = client.channels.cache.get('747750993583669258')
+
+      message.channel.send(`Whoops, We got a error right now! This error has been reported to Support center!`)
+
+      errorlogs.send(`Error on bs commands!\n\nError:\n\n ${err}`)
+
+    }
+  }
+};
