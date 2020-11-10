7abd3832799693cfbf52eae8360d445a348c034a
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-11-10 10:39:15 +0530

    Updated

--- Commands/Tickets/new.js
+++ Commands/Tickets/new.js
@@ -5,7 +5,9 @@ module.exports = {
   description: "makes a ticket channel!",
   cooldown: 3,
   run: async(client, message, args)=> {
-    const guildDoc = await guildModel.findOne({ Guild: message.guild.id });
+    const guildDoc = await guildModel.findOne({ 
+    Guild: message.guild.id 
+    });
     const e = message.member;
     const user = await message.guild.members.cache.get(e.id);
     await createTicket(message, user, guildDoc);
