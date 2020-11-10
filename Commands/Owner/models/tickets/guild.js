5c2f814f7711cb7c54c3407880bcbdc6f9bec9cb
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-11-10 10:49:09 +0530

    Updated

--- Commands/Owner/models/tickets/guild.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const guildModel = mongoose.model(
-  "Guild",
-  new mongoose.Schema({
-    Guild: String,
-    tickets: { type: Number, default: 0 },
-  })
-);
-module.exports = guildModel;
--- Commands/Owner/models/tickets/panel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const panelModel = mongoose.model(
-  "panel",
-  new mongoose.Schema({
-    guild: String,
-    msg: String,
-  })
-);
-module.exports = panelModel;
