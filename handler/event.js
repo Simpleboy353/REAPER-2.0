7b63a7414e7755f137022b271d08777508559a5f
Simpleboy353 <67364262+Simpleboy353@users.noreply.github.com>
2020-11-11 17:41:40 +0530

    Updated

--- handler/event.js
+++ handler/event.js
@@ -1,12 +1,13 @@
+const { load } = require("dotenv/types");
 const {readdirSync} = require("fs");
 module.exports=(client)=> {
   const load = dirs => {
-    const events = readdirSync(`../events/${dirs}/`).filter(d=>d.endsWith(".js"));
-    for (let file of events){
-      const evt = require(`../events/${dirs}/${file}`);
+    const events = readdirSync(`./events/${dirs}/`).filter(f => f.endsWith('.js'));
+    for (let file of events) {
+      let evt = require(`../events/${dirs}/${file}`);
       let eName = file.split(".")[0];
       client.on(eName, evt.bind(null, client))
     };
-  };
-  ["client", "guild"].forEach(x=>load(x));
+   };
+  ["client", "guild"].forEach(x=>load(x))
 };
