3e020cb859126383e5f9038c4079b824202ff3d3
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2020-11-11 14:11:36 +0530

    Updated

--- Commands/Info/invite.js
+++ Commands/Info/invite.js
@@ -8,7 +8,7 @@ module.exports = {
         let embed = new Discord.MessageEmbed()
         .setTitle("Invite Me")
         .setColor("RANDOM")
-            .setDescription("**Get Inifinity's Invite Link [Here](https://discord.com/oauth2/authorize?client_id=733670294086221865&permissions=1584921983&scope=bot)**\n**Need assistance? Join our [Support Server](https://discord.gg/mqWprFc) now!**")
+            .setDescription("**Get Infinity's Invite Link [Here](https://discord.com/oauth2/authorize?client_id=733670294086221865&permissions=1584921983&scope=bot)**\n**Need assistance? Join our [Support Server](https://discord.gg/mqWprFc) now!**")
         .setFooter(`Requested By: ${message.author.username}`);
         message.channel.send(embed);
     }
