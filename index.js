4e6252b3364b1bbb23c430cb7cc28781ec1eb8f6
AgentRockstar <67364262+Simpleboy353@users.noreply.github.com>
2021-01-10 13:47:41 +0530

    Auto-Role is here!

--- index.js
+++ index.js
@@ -52,4 +52,26 @@ client.prefix = prefix;
 
 }
 });
+
+// Auto-Role is here!
+
+client.on("guildMemberAdd", async(member)=>{
+  const roleData = require("./database/guildData/autorole")
+  const data = await roleData.findOne({
+      GuildID: member.guild.id,
+  }).catch(err=>console.log(err));
+  
+  if (data) {
+      let role = data.Role;
+      let arole = member.guild.roles.cache.get(role);
+    if (role) {
+      member.roles.add(arole)
+    } else if (!role) {
+        return;
+    }
+  } else if (!data) {
+      return;
+  }
+});
+
 client.login(process.env.token)//Enter your bot token here
