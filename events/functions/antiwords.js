cc20735713b01e685205a07ebfe4d5b064523aa7
Brandon Torreglosa <84269666+HACKERPROTM@users.noreply.github.com>
2021-07-20 15:50:42 +0300

    Delete antiwords.js

--- events/functions/antiwords.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const antiwordsSchema = new mongoose.Schema({
-    GuildID: String,
-});
-
-const model = mongoose.model('antiwords', antiwordsSchema);
-
-module.exports = model;
