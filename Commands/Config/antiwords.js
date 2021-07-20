0a6e8b9617a208a2b1ebb1a89732e796ca4a35db
Brandon Torreglosa <84269666+HACKERPROTM@users.noreply.github.com>
2021-07-20 15:46:38 +0300

    Add files via upload

---
+++ Commands/Config/antiwords.js
@@ -0,0 +1,53 @@
+const Discord = require("discord.js")
+const antiwordsSchema = require("../models/antiwords");
+
+module.exports = {
+  name: "antiwords",
+  permissions: ["ADMINISTRATOR"],
+  description: "Setup antilink per server!",
+  async execute(client, message, cmd, args, Discord) {
+    if (!args[0]) {
+      return message.channel.send(`Usage: \`(prefix)antiwords <on|off>\``)
+    }
+    if (args[0] === "On" || args[0] === "on") {
+      const data = await antiwordsSchema.findOne({
+        GuildID: message.guild.id,
+      });
+
+      if (data) {
+        await antiwordsSchema.findOneAndRemove({
+          GuildID: message.guild.id,
+        });
+
+        message.channel.send(`**Antiwords is now active!**`);
+
+        let newData = new antiwordsSchema({
+          GuildID: message.guild.id,
+        });
+        newData.save();
+      } else if (!data) {
+        message.channel.send(`**Antiwords is now active**`);
+
+        let newData = new antiwordsSchema({
+          GuildID: message.guild.id,
+        });
+        newData.save();
+      }
+    } else if (args[0] === "Off" || args[0] === "off") {
+      const data2 = await antiwordsSchema.findOne({
+        GuildID: message.guild.id,
+      });
+
+      if (data2) {
+        await antiwordsSchema.findOneAndRemove({
+          GuildID: message.guild.id,
+        });
+
+        return message.channel.send(`**Antiwords has been turned off!**`);
+
+      } else if (!data2) {
+        return message.channel.send(`**Antiwords Isn't Even Setup Bot!**`);
+      }
+    }
+  }
+}
