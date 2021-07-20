00ded5c97023d4b39b5cea334f63730be0da6142
Brandon Torreglosa <84269666+HACKERPROTM@users.noreply.github.com>
2021-07-20 15:43:35 +0300

    Update dockofshame.js

--- Commands/Image/dockofshame.js
+++ Commands/Image/dockofshame.js
@@ -7,18 +7,10 @@ module.exports = {
     cooldown: 3,
     description: "Image Manipulation Command",
     async execute(client, message, cmd, args, Discord) {
-
-        //     const user = message.mentions.members.first()
-        // if (!user) {
-        //     return message.channel.send("**Please Mention A User!**")
-        // }
-
-        // const avatar = user.user.displayAvatarURL({ size: 2048, format: "png" });
-
         const mention = message.mentions.members.first() || message.member;
         const avatar = mention.user.displayAvatarURL({ size: 2048, format: "png" });
 
 
-        message.channel.send({ files: [{ attachment: `https://vacefron.nl/api/dockofshame?user=${avatar}`, name: "xopbotdockofshame.png" }] });
+        message.channel.send({ files: [{ attachment: `https://vacefron.nl/api/dockofshame?user=${avatar}`, name: "reaperdockofshame.png" }] });
     }
 }
