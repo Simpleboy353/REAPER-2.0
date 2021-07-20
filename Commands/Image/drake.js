051baeb3e45307699b1738356ad3ff217141dad9
Brandon Torreglosa <84269666+HACKERPROTM@users.noreply.github.com>
2021-07-20 15:44:23 +0300

    Update drake.js

--- Commands/Image/drake.js
+++ Commands/Image/drake.js
@@ -18,14 +18,12 @@ module.exports = {
         if (!text) {
             return message.channel.send("**Enter Some Text!**")
         }
-        // if (text.length > 5) return message.channel.send('**You Are Not Allowed To Go Over 15 Characters!**');
 
         const text2 = args.slice(2).join(" ");
         if (!text2) {
             return message.channel.send("**Enter The Second Text!**")
         }
-        // if (text2.length > 5) return message.channel.send('**You Are Not Allowed To Go Over 15 Characters!**');
 
-        message.channel.send({ files: [{ attachment: `https://api.popcatdev.repl.co/drake?text1=${text}&text2=${text2}`, name: "xopbotdrake.png" }] });
+        message.channel.send({ files: [{ attachment: `https://api.popcatdev.repl.co/drake?text1=${text}&text2=${text2}`, name: "reaperdrake.png" }] });
     }
 }
