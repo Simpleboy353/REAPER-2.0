984835b74a79050a34966cc95e9645904c2337b8
Brandon Torreglosa <84269666+HACKERPROTM@users.noreply.github.com>
2021-07-20 15:53:34 +0300

    Create antiwords.js

---
+++ events/functions/antiwords.js
@@ -0,0 +1,21 @@
+const antiwordsData = require('../../database/guildData/antiwords')
+  client.on("message", async (message) => {
+    const antiwords = await antiwordsData.findOne({
+      GuildID: message.guild.id,
+    })
+    if (antiwords) {
+      if (message.content.match("bitch") || message.content.match("hoe") || message.content.match("slut") || message.content.match("nigga") || message.content.match("nigg") || message.content.match("dick") || message.content.match("cunt") || message.content.match("shit") || message.content.match("fuck")) {
+        message.delete();
+        message.reply("**No Bad Words Allowed Please Stop!**").then(msg => {
+          let time = '4s'
+          setTimeout(function () {
+            msg.delete();
+          }, ms(time));
+        })
+      } else {
+        return;
+      }
+    } else if (!antiwords) {
+      return;
+    }
+  });
