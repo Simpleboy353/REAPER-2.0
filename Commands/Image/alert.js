a05c55c6b5521f70cb4dc0d7e4eee03bfd7f91ad
Brandon Torreglosa <84269666+HACKERPROTM@users.noreply.github.com>
2021-07-20 15:42:28 +0300

    Update alert.js

--- Commands/Image/alert.js
+++ Commands/Image/alert.js
@@ -12,6 +12,6 @@ module.exports = {
     let alertMessage = args.slice(0).join(' ');
     if (alertMessage.length > 65) return message.channel.send('**You Are Not Allowed To Go Over 65 Characters!**');
 
-    message.channel.send({ files: [{ attachment: `https://api.popcatdev.repl.co/alert?text=${alertMessage}`, name: 'xopbotalert.jpg' }] });
+    message.channel.send({ files: [{ attachment: `https://api.popcatdev.repl.co/alert?text=${alertMessage}`, name: 'reaperalert.jpg' }] });
   }
 }
