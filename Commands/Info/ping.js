7a51f90952de48d1d8df98c36546cf62a9d9eb36
°『ᎢᏟ 』•ᴮᴬᴰʙᴏʏツ <67364262+Simpleboy353@users.noreply.github.com>
2021-08-26 07:16:52 +0530

    Update ping.js
    Fixed Ping Command Errors

--- Commands/Info/ping.js
+++ Commands/Info/ping.js
@@ -1,28 +1,33 @@
 const discord = require("discord.js");
 
 module.exports = {
-  name: "ping",
-  aliases: ["pong"],
-  category: "Utility",
-  usage: "ping",
-  description: "Get the bot's ping!",
-  botPerms: ["EMBED_LINKS"],
-  run: async (client, message, args) => {
-    let start = Date.now();
+    name: "ping",
+    aliases: ["pong"],
+    category: "Utility",
+    usage: "ping",
+    description: "Get the bot's ping!",
+    ownerOnly: false,
+    nsfwOnly: false,
+    run: async (client, message, args) => {
 
-    message.channel
-      .send({
-        embed: { description: "Looks like the bot is slow.", color: "RANDOM" },
-      })
-      .then((m) => {
-        let end = Date.now();
+     let start = Date.now();
 
-        let embed = new discord.MessageEmbed()
-          .setAuthor("Ping!", message.author.avatarURL())
-          .addField("API Latency", Math.round(client.ws.ping) + "ms", true)
-          .addField("Message Latency", end - start + "ms", true)
-          .setColor("RANDOM");
-        m.edit({ embeds: [embed] }).catch((e) => message.channel.send(e));
-      });
-  },
+     let pingEmbed = new discord.MessageEmbed()
+     .setDescription("Looks like the bot is slow.")
+     .setColor("RANDOM")
+  
+  message.channel.send({ embeds: [pingEmbed] }).then(m => {
+    
+    let end = Date.now();
+    
+    let embed = new discord.MessageEmbed()
+    .setAuthor("Ping!", message.author.avatarURL())
+    .addField("API Latency", Math.round(client.ws.ping) + "ms", true)
+    .addField("Message Latency", end - start + "ms", true)
+    .setColor("RANDOM");
+    m.edit({ embeds: [embed] }).catch(e => message.channel.send(e))
+    
+  })
+
+    }
 };
