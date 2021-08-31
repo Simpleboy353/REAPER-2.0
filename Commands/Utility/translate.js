ac0831acd64b86109023f1ae4b336ea48ba7fe2c
InsufiKoder <87572099+InsufiKoder@users.noreply.github.com>
2021-08-31 22:49:00 +0300

    Move calculator, add translate and animesearch.js

---
+++ Commands/Utility/translate.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+const translate = require('@iamtraction/google-translate');
+
+module.exports = {
+  name: "translate",
+  description: "Translates the given message.",
+  run: async (client, message, args) => {
+        const txt = args.slice(1).join(" ")
+        const lang = args[0]
+        if(!lang) return message.channel.send("Provide the ISO code of the language.")
+        if(!txt) return message.channel.send("Provide a text to translate.")
+        translate(txt, { to: lang }).then(res => {
+          const embed = new discord.MessageEmbed()
+          .setDescription(res.text)
+          .setColor("RANDOM")
+          message.channel.send({ embeds: [embed] });
+    }).catch(err => {
+      message.channel.send("Please provide a valid ISO language code.")
+    });
+  },
+};
