ee75c39e1362a8ed493359c9f049931a3d54ec3a
InsufiKoder <87572099+InsufiKoder@users.noreply.github.com>
2021-08-31 14:19:07 +0300

    fix mute and unmute.js

--- Commands/Mod/unmute.js
+++ Commands/Mod/unmute.js
@@ -1,32 +1,24 @@
-const { MessageEmbed } = module.require("discord.js");
+const { Client, Message, MessageEmbed } = require('discord.js');
 
 module.exports = {
-  name: "unmute",
-  description: "Unmute members in one shot",
-  category: "moderation",
-  usage: "=ummute <@user> <reason >",
-  userPerms: ["MANAGE_ROLES"],
-  botPerms: ["EMBED_LINKS", "MANAGE_ROLES"],
-  run: async (client, message, args) => {
-    const user = message.mentions.members.first();
+    name: 'unmute',
+    aliases: [''],
+    description: 'Unmutes the specified user.',
+    usage: '',
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args, Discord) => {
 
-    if (!user) {
-      return message.channel.send(
-        `**${message.author.username}**, Please mention the member who you want to unmute`
-      );
+        const member = message.mentions.members.first();
+        let target = message.guild.members.cache.get(member.id)
+        const role = message.guild.roles.cache.find(role => role.name === 'Muted')
+
+        target.roles.remove(role.id);
+        message.reply('Unmuted!')
+
+
     }
-
-    let muterole = message.guild.roles.cache.find((x) => x.name === "Muted");
-
-    if (user.roles.cache.has(muterole)) {
-      return message.channel.send(`Given User is not muted`);
-    }
-    user.roles.remove(muterole);
-
-    const embed = new MessageEmbed()
-      .setTitle("Unmute")
-      .setColor("RANDOM")
-      .setDescription(`User Unmuted: ${user} \nUnmuted by: ${message.member}`);
-    message.channel.send({ embeds: [embed] });
-  },
-};
+}
