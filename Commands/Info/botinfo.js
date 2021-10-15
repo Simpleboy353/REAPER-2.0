86251c92a8bcf30749e9e0e6cb15a657624dffeb
°『ᎢᏟ 』•ᴮᴬᴰʙᴏʏツ <67364262+Simpleboy353@users.noreply.github.com>
2021-10-15 07:00:55 +0530

    Fixed Bugs

--- Commands/Info/botinfo.js
+++ Commands/Info/botinfo.js
@@ -27,7 +27,7 @@ module.exports = {
           client.users.cache.size
         } \n**Servers:** ${client.guilds.cache.size} \n**Channels:** ${
           client.channels.cache.size
-        }
+        }`
       )
       .addField(
         "About Reaper-2.0",
