d3da66fc45a1ab1413bc7189fc13788e4ce940ed
Nathan K <92893096+Natesplanet@users.noreply.github.com>
2021-11-09 18:07:41 -0400

    Update for Ass.js (FIX)
    The link in ass.js is not working anymore so I added pics from Reddit :)

--- Commands/NSFW/ass.js
+++ Commands/NSFW/ass.js
@@ -1,43 +1,46 @@
-const superagent = module.require("node-fetch");
-const Discord = require('discord.js')
-
-const rp = require('request-promise-native');
+const discord = require("discord.js");
+const got = require("got"); //MAKE SURE TO INSTALL THE PACKAGE "GOT" ELSE THE CODE WOULD NOT WORK
 
 module.exports = {
-  name: "ass",
+  name: "4k",
   category: "NSFW",
-  description: "Sends ass",
+  description: "Sends 4k girl pics",
+  usage: "[command]",
   botPerms: ["EMBED_LINKS"],
-  run: async (client, message, args, level) => {
-    //command
+  run: async (client, message, args) => {
+    try {
+      //command
+      var errMessage = "This is not an NSFW Channel";
+      if (!message.channel.nsfw) {
+        message.react("💢");
 
-    //Checks channel for nsfw
-    var errMessage = "This is not an NSFW Channel";
-    if (!message.channel.nsfw) {
-      message.react("💢");
-
-      return message.reply(errMessage).then((msg) => {
-        setTimeout(() => msg.delete(), 3000);
-      });
-    }
-
-    return rp
-      .get("http://api.obutts.ru/butts/0/1/random")
-      .then(JSON.parse)
-      .then(function (res) {
-        return rp.get({
-          url: "http://media.obutts.ru/" + res[0].preview,
-          encoding: null,
+        return message.reply(errMessage).then((msg) => {
+          setTimeout(() => msg.delete(), 3000);
         });
-      })
-      .then(function (res) {
-        const ass = new Discord.MessageEmbed()
-          .setTitle("Ass")
-          .setColor(`#FF0000`)
-          .setImage("attachment://file.png")
-          .attachFiles([{ attachment: res, name: "file.png" }]);
+      }
+      got("https://www.reddit.com/r/Asshole/random.json")
+        .then((response) => {
+          let content = JSON.parse(response.body);
+          var title = content[0].data.children[0].data.title;
+          var amazeme = content[0].data.children[0].data.url;
+          let wow = new discord.MessageEmbed()
+            .setDescription(`**${title}**`)
+            .setImage(amazeme)
+            .setFooter(`Nice `)
+            .setColor("RANDOM");
+          message.channel.send({ embeds: [wow] });
+        })
+        .catch(console.error);
+    } catch (err) {
+      const errorlogs = client.channels.cache.get("747423875956080801");
 
-        message.channel.send({embeds: [ass]});
-      });
+      message.channel.send(
+        `Whoops, We got a error right now! This error has been reported to Support center!`
+      );
+
+      errorlogs.send(
+        `Error in ${message.guild.name}  by ${message.author.username} on  Asshole commands!\n\nError:\n\n ${err}`
+      );
+    }
   },
 };
