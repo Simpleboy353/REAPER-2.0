f6d654a9a4ca1b4c1030d6b993579e7e5b27dc93
Zachary Lannon <Zlannon@yahoo.com>
2022-09-13 02:49:28 -0400

    fixed addfield bug

--- Commands/Utility/wiki.js
+++ Commands/Utility/wiki.js
@@ -4,18 +4,20 @@ module.exports = {
   name: "wiki",
   description: "Get Search Results from Wikipedia",
   botPerms: ["EmbedLinks"],
-  run: async (client, message, args) => {
-    const search = args.join("_");
-    const msg = args.join(" ");
-    if (!msg) {
-      return message.channel.send("You need to enter some text to search for");
-    }
-    const link = `https://www.wikipedia.org/w/index.php?search=${search}&ns0=1`;
-    const embed = new Discord.EmbedBuilder()
-      .setTitle("Wikipedia Search")
-      .addField(`You Searched for:`, `${msg}`)
-      .addField(`Results:`, `[Here's What I found](${link})`)
-      .setColor("Random");
+    run: async (client, message, args) => {
+        const search = args.join("_");
+        const msg = args.join(" ");
+        if (!msg) {
+            return message.channel.send("You need to enter some text to search for");
+        }
+        const link = `https://www.wikipedia.org/w/index.php?search=${search}&ns0=1`;
+        const embed = new Discord.EmbedBuilder()
+            .setTitle(`Wikipedia Search`)
+            .addFields(
+                { name: `You Searched for:`, value: `${msg}`},
+                { name: `Results:`, value: `[Here's What I found](${link})` },
+            )
+            .setColor("Random");
 
     message.channel.send({ embeds: [embed] });
   },
