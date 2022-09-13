8a3684347590c5f27c9c6501ba18533b3980e834
Zachary Lannon <Zlannon@yahoo.com>
2022-09-13 02:48:41 -0400

    fixed permission update bug

--- Commands/Utility/unlock.js
+++ Commands/Utility/unlock.js
@@ -1,4 +1,5 @@
 const Discord = module.require("discord.js");
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
   name: "unlock",
@@ -6,17 +7,15 @@ module.exports = {
   userPerms: ["ManageChannels"],
   botPerms: ["EmbedLinks", "ManageChannels", "ManageMessages"],
   run: async (client, message, args) => {
-    message.channel.overwritePermissions([
-      {
-        id: message.guild.id,
-        null: ["SEND_MESSAGES"],
-      },
-    ]);
-    const embed = new Discord.EmbedBuilder()
-      .setTitle("Channel Updates")
-      .setDescription(`🔓 ${message.channel}  has been Unlocked`)
-      .setColor("Random");
-    await message.channel.send({ embeds: [embed] });
-    message.delete();
+      const role = message.guild.roles.cache.find(r => r.name === '@everyone')
+
+      message.channel.permissionOverwrites.create(role, { SendMessages: true });
+
+      const embed = new Discord.EmbedBuilder()
+          .setTitle("Channel Updates")
+          .setDescription(`🔓 ${message.channel}  has been Unlocked`)
+          .setColor("Random");
+      await message.channel.send({ embeds: [embed] });
+      message.delete();
   },
 };
